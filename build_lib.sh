71ac732031f1012790cac5beb49909487e6904e9
Alexander <0oktonion0@gmail.com>
2018-04-05 23:10:58 +0300

    library build

---
+++ build_lib.sh
@@ -0,0 +1,26 @@
+mkdir ./obj
+
+build_ok = 0
+
+for file in ./src/*.cpp; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "compiling $filename"
+  if ! $COMPILER -o "./obj/$filename.o" $file; then
+    build_ok = 1
+  fi
+done
+
+if ! build_ok; then
+  exit 1
+fi
+
+mkdir ./lib
+
+for file in ./obj/*.o; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "compiling $filename"
+  ar rcs ./lib/libstdex.a $file
+done
+
