5d9de8954da9fea712b85c20cb8c2ad97dae515c
Alexander <0oktonion0@gmail.com>
2018-04-05 22:55:56 +0300

    fail test

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,10 @@ for file in ./tests/bin/*; do
   chmod +x "$file"
   filename=$(basename -- "$file")
   filename=$([[ "$filename" = *.* ]] && echo "${filename%.*}" || echo "$filename")
-  echo "running test $filename"
-  "$file"
+  echo "running test $filename..."
+  if ! "$file"; then
+    echo "...failed."
+  else
+    echo "...ok."
+  fi 
 done
