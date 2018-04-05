e9a3afff68e2e01a2f5b1e39c6eaf7e09dabeaad
Alexander <0oktonion0@gmail.com>
2018-04-05 23:52:49 +0300

    ffff

--- build_lib.sh
+++ build_lib.sh
@@ -1,17 +1,17 @@
 mkdir ./obj
 
-build_ok=0
+build_ok=1
 
 for file in ./src/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling $filename"
   if ! $COMPILER -c "$file" -o "./obj/$filename.o"; then
-    build_ok=1
+    build_ok=0
   fi
 done
 
-if [ $build_ok ]; then
+if [ $build_ok -eq 0 ]; then
   echo "libstdex.a build failed"
   exit 1
 fi
