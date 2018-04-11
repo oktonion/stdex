7e073fc6621f8215ff5814ba630455f75da77787
Alexander <0oktonion0@gmail.com>
2018-04-11 23:24:45 +0300

    merge to work

---
+++ merge_to_work.sh
@@ -0,0 +1,12 @@
+git checkout tmp
+git merge -m "merging tested lib" origin/tests
+
+git filter-branch --prune-empty --subdirectory-filter stdex --
+git commit -m 'merged with tested lib'
+
+git checkout work
+git fetch origin
+git merge --allow-unrelated-histories --no-commit origin/tmp
+git rm -r stdex
+git read-tree --prefix=stdex -u origin/tmp
+git commit -m 'merged with tested lib'
