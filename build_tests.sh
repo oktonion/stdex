003f56d2d8b794f3d9bff940deb6ac77eef4abf0
Alexander <0oktonion0@gmail.com>
2018-04-12 21:36:07 +0300

    building for c++98 also

--- build_tests.sh
+++ build_tests.sh
@@ -13,5 +13,19 @@ done
 
 if [ $build_ok -eq 0 ]; then
   echo "tests build failed"
-  exit 1
+  exit 3
+fi
+
+for file in ./tests/*.cpp; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "compiling test $filename"
+  if ! $COMPILER -std=98 -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
+    build_ok=0
+  fi
+done
+
+if [ $build_ok -eq 0 ]; then
+  echo "tests build failed for c++98"
+  exit 98
 fi
