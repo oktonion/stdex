310813af1e2ec2b1f211b598ac4d5b35c289a662
oktonion <0oktonion0@gmail.com>
2018-04-12 17:37:42 +0300

    c++03

--- build_tests.sh
+++ build_tests.sh
@@ -3,7 +3,7 @@ for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test $filename"
-  if ! $COMPILER -std=c++98 -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
+  if ! $COMPILER -std=c++0x -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
     build_ok=0
   fi
 done
