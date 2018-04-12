c110816b276267dfa92656c58a394281fd5fab7e
oktonion <0oktonion0@gmail.com>
2018-04-12 17:30:47 +0300

    check for build result

--- build_tests.sh
+++ build_tests.sh
@@ -3,5 +3,12 @@ for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test $filename"
-  $COMPILER -std=c++98 -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file
+  if ! $COMPILER -std=c++98 -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
+    build_ok=0
+  fi
 done
+
+if [ $build_ok -eq 0 ]; then
+  echo "tests build failed"
+  exit 1
+fi
