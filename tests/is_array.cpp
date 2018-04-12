48092633c71ff5f205eeccd8cf8c97a114dea4ea
oktonion <0oktonion0@gmail.com>
2018-04-12 16:51:20 +0300

    is_array test

---
+++ tests/is_array.cpp
@@ -0,0 +1,22 @@
+#include "../stdex/include/core.h"
+#include "../stdex/include/type_traits.hpp"
+
+struct ClassType {};
+
+int main(void)
+{
+    using namespace stdex;
+    
+    // is_array
+    STATIC_ASSERT(is_array<int[2]>::value == (true), should_be_array);
+    STATIC_ASSERT(is_array<int[2][3]>::value == (true), should_be_array);
+    STATIC_ASSERT(is_array<float*[2]>::value == (true), should_be_array);
+    STATIC_ASSERT(is_array<float*[2][3]>::value == (true), should_be_array);
+    STATIC_ASSERT(is_array<ClassType[2]>::value == (true), should_be_array);
+    STATIC_ASSERT(is_array<ClassType[2][3]>::value == (true), should_be_array);
+
+    // Sanity check.
+    STATIC_ASSERT(is_array<ClassType>::value == (false), can_not_be_array);
+
+    return 0;
+}
