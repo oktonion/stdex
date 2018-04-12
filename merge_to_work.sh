fcd6668536ddeb73cc134dfeef35f86c024c8e93
oktonion <0oktonion0@gmail.com>
2018-04-12 09:57:00 +0300

    script fix

--- merge_to_work.sh
+++ merge_to_work.sh
@@ -2,12 +2,14 @@ git checkout .
 git remote update
 git fetch
 git checkout -b tmp
+git checkout tmp
 git merge -m "merging tested lib" origin/tests
 
 git filter-branch --prune-empty --subdirectory-filter stdex --
 git commit -m 'merged with tested lib'
 
 git checkout -b work
+git checkout work
 git fetch origin
 git merge --allow-unrelated-histories --no-commit origin/tmp
 git rm -r stdex
