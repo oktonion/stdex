dbe8d7abf9cca83b7f74f540c5d295ba1d1ece24
Alexander <0oktonion0@gmail.com>
2018-04-12 22:00:26 +0300

    c++98 diff

--- build_tests.sh
+++ build_tests.sh
@@ -5,7 +5,7 @@ build_ok=1
 for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
-  echo "compiling test $filename"
+  echo "compiling test c++0x $filename"
   if ! $COMPILER -std=c++0x -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
     build_ok=0
   fi
@@ -19,7 +19,7 @@ fi
 for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
-  echo "compiling test $filename"
+  echo "compiling test c++98 $filename"
   if ! $COMPILER -std=c++98 -L./stdex/lib/ -lstdex -o "./tests/bin/$filename" $file; then
     build_ok=0
   fi
