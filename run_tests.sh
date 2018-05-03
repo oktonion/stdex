0bbf28c9262d6aab36a9d11ce0f2fa260332d467
oktonion <0oktonion0@gmail.com>
2018-05-03 18:38:36 +0300

    better fail summary

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,16 @@
+run_ok=0
 for file in ./tests/bin/*; do
   chmod +x "$file"
   filename=$(basename -- "$file")
   filename=$([[ "$filename" = *.* ]] && echo "${filename%.*}" || echo "$filename")
   echo "running test $filename..."
-  if ! "$file"; then
-    echo "...failed."
+  run_result="$file"
+  if ! run_result; then
+    run_ok=-1
+    echo "...failed at line $run_result."
   else
     echo "...ok."
   fi 
 done
+
+return run_ok
