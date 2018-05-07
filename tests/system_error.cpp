783f057a5bb532ebea51f298e17cd730e71b2fa5
oktonion <0oktonion0@gmail.com>
2018-05-07 10:30:47 +0300

    removed temp fix for system_error tests

--- tests/system_error.cpp
+++ tests/system_error.cpp
@@ -20,25 +20,25 @@ public:
 	std::string message(int) const { return ""; }
 } my_error_category_instance;
 
-//namespace stdex
-//{
+namespace stdex
+{
 	stdex::error_code
 		make_error_code(my_errc e)
 	{
 		return stdex::error_code(static_cast<int>(e),
 			my_error_category_instance);
 	}
-//}
+}
 
-//namespace stdex
-//{
+namespace stdex
+{
 	stdex::error_condition
 		make_error_condition(my_errc e)
 	{
 		return stdex::error_condition(static_cast<int>(e),
 			my_error_category_instance);
 	}
-//}
+}
 
 namespace stdex
 {
