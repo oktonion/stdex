b86a9075d15c28382c3aed1e76fbfb8bd9f57814
oktonion <0oktonion0@gmail.com>
2018-05-08 10:15:32 +0300

    - warn of c++11 long long

--- build_tests.sh
+++ build_tests.sh
@@ -6,7 +6,7 @@ for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test c++03 $filename"
-  if ! $COMPILER -std=c++03 -pedantic $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
+  if ! $COMPILER -std=c++03 -pedantic -Wno-c++11-long-long $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
     build_ok=0
   fi
 done
@@ -20,7 +20,7 @@ for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test c++98 $filename"
-  if ! $COMPILER -std=c++98 -pedantic $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
+  if ! $COMPILER -std=c++98 -pedantic -Wno-c++11-long-long $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
     build_ok=0
   fi
 done
