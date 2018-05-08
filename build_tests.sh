fe833a27b8bec8f3b80be487e2aa7ff570940c6d
oktonion <0oktonion0@gmail.com>
2018-05-08 10:22:22 +0300

    exclude warnings for specific compiler

--- build_tests.sh
+++ build_tests.sh
@@ -1,12 +1,18 @@
 mkdir ./tests/bin
 
 build_ok=1
+exclude_warn=""
+
+if [[ $COMPILER = *"clang"* ]]; then
+  exclude_warn=-Wno-c++11-long-long
+else
+  exclude_warn=-Wlong-long
 
 for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test c++03 $filename"
-  if ! $COMPILER -std=c++03 -pedantic -Wno-c++11-long-long $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
+  if ! $COMPILER -std=c++03 -pedantic $exclude_warn $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
     build_ok=0
   fi
 done
@@ -20,7 +26,7 @@ for file in ./tests/*.cpp; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   echo "compiling test c++98 $filename"
-  if ! $COMPILER -std=c++98 -pedantic -Wno-c++11-long-long $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
+  if ! $COMPILER -std=c++98 -pedantic $exclude_warn $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
     build_ok=0
   fi
 done
