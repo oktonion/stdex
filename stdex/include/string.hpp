abc94e0298a980e00ac0ae52c61ad7d061150ac4
oktonion <0oktonion0@gmail.com>
2018-05-17 11:31:53 +0300

    + string long double conversions

--- stdex/include/string.hpp
+++ stdex/include/string.hpp
@@ -90,6 +90,48 @@ namespace stdex
 			};
 		};
 
+		template<class _T, bool _HasQuietNaN, bool _HasSignalingNaN>
+		struct _not_a_number_impl
+		{
+			static _T NaN() { return std::numeric_limits<_T>::quiet_NaN();}
+		};
+
+		template<class _T>
+		struct _not_a_number_impl<_T, false, true>
+		{
+			static _T NaN() { return std::numeric_limits<_T>::signaling_NaN(); }
+		};
+
+		template<class _T>
+		struct _not_a_number_impl<_T, false, false>
+		{
+			static _T NaN() { return (_T)(); }
+		};
+
+		template<class _T>
+		struct _not_a_number
+		{
+			typedef _not_a_number_impl<_T, std::numeric_limits<_T>::has_quiet_NaN, std::numeric_limits<_T>::has_signaling_NaN> impl;
+		};
+
+		template<class _T, bool _HasInfinity>
+		struct _infinity_impl
+		{
+			static _T inf() { return std::numeric_limits<_T>::infinity(); }
+		};
+
+		template<class _T>
+		struct _infinity_impl<_T, false>
+		{
+			static _T inf() { return std::numeric_limits<_T>::max(); }
+		};
+
+		template<class _T>
+		struct _infinity
+		{
+			typedef _infinity_impl<_T, std::numeric_limits<_T>::has_infinity> impl;
+		};
+
 #ifdef LLONG_MAX
 		template<bool _IsSigned>
 		struct _str_to_integral_chooser_impl_ll
@@ -282,6 +324,165 @@ namespace stdex
 
 			return _value;
 		}
+#else
+		long double
+			_a_to_floating_point(const char *str)
+		{
+			using namespace std;
+
+			long double value;
+
+			if (sscanf(str, "%Lf", &value) == EOF)
+			{
+
+			  long double fp_integer_part = 0.0L, fp_fractional_part = 0.0L;
+			  size_t i, length = strlen(str);
+
+			  i = 0; // Left to right
+			  while (str[i] != '.') {
+				  fp_integer_part = fp_integer_part * 10.0L + (str[i] - '0');
+				  i++;
+			  }
+
+			
+			  i = length - 1; // Right to left
+
+			  while (!isdigit(str[i]) && str[i] != '.')
+				 i--;
+
+			  while (str[i] != '.') {
+				  fp_fractional_part = (fp_fractional_part + (str[i] - '0')) / 10.0L;
+				  i--;
+			  }
+
+			  value = fp_integer_part + fp_fractional_part;
+			}
+
+			if(value > numeric_limits<long double>::max() || value < numeric_limits<long double>::min())
+			{
+			  errno = ERANGE;
+			  value = 0.0;
+			}
+
+			return value;
+		}
+
+		long double
+			_cs_to_long_double(const char *str, char const **ptr)
+		{
+			using namespace std;
+
+			const char *p;
+
+			if (!ptr)
+				return _a_to_floating_point(str);
+
+			p = str;
+
+			while (isspace(*p))
+				++p;
+
+			if (*p == '+' || *p == '-')
+				++p;
+
+			typedef _not_a_number<long double>::impl not_a_number_impl;
+			typedef _infinity<long double>::impl infinity_impl;
+
+			/* INF or INFINITY.  */
+			if ((p[0] == 'i' || p[0] == 'I')
+				&& (p[1] == 'n' || p[1] == 'N')
+				&& (p[2] == 'f' || p[2] == 'F'))
+			{
+				if ((p[3] == 'i' || p[3] == 'I')
+					&& (p[4] == 'n' || p[4] == 'N')
+					&& (p[5] == 'i' || p[5] == 'I')
+					&& (p[6] == 't' || p[6] == 'T')
+					&& (p[7] == 'y' || p[7] == 'Y'))
+				{
+					*ptr = p + 8;
+					return infinity_impl::inf();
+				}
+				else
+				{
+					*ptr = p + 3;
+					return infinity_impl::inf();
+				}
+			}
+
+			/* NAN or NAN(foo).  */
+			if ((p[0] == 'n' || p[0] == 'N')
+				&& (p[1] == 'a' || p[1] == 'A')
+				&& (p[2] == 'n' || p[2] == 'N'))
+			{
+				p += 3;
+				if (*p == '(')
+				{
+					++p;
+					while (*p != '\0' && *p != ')')
+						++p;
+					if (*p == ')')
+						++p;
+				}
+				*ptr = p;
+				return not_a_number_impl::NaN();
+			}
+
+			/* digits, with 0 or 1 periods in it.  */
+			if (isdigit(*p) || *p == '.')
+			{
+				int got_dot = 0;
+				while (isdigit(*p) || (!got_dot && *p == '.'))
+				{
+					if (*p == '.')
+						got_dot = 1;
+					++p;
+				}
+
+				/* Exponent.  */
+				if (*p == 'e' || *p == 'E')
+				{
+					int i;
+					i = 1;
+					if (p[i] == '+' || p[i] == '-')
+						++i;
+					if (isdigit(p[i]))
+					{
+						while (isdigit(p[i]))
+							++i;
+						*ptr = p + i;
+						if (std::numeric_limits<long double>::max_exponent10 < i)
+						{
+							errno = ERANGE;
+							return std::numeric_limits<long double>::max();
+						}
+						return _a_to_floating_point(str);
+					}
+				}
+				*ptr = p;
+				return _a_to_floating_point(str);
+			}
+			/* Didn't find any digits.  Doesn't look like a number.  */
+			*ptr = str;
+			return not_a_number_impl::NaN();
+		}
+
+		template <>
+		inline long double _cs_to_floating_point<long double>(const char *str, const char *&num_s_end)
+		{
+			using namespace std;
+
+			int last_errno = errno;
+			errno = 0;
+			long double _value = _cs_to_long_double(str, &num_s_end);
+
+			if (errno == ERANGE)
+				num_s_end = 0;
+
+			if (errno != last_errno)
+				errno = last_errno;
+
+			return _value;
+		}
 #endif
 	}
 
@@ -376,7 +577,7 @@ namespace stdex
 		double _value = static_cast<double>(detail::_cs_to_floating_point<double>(_ptr, _eptr));
 
 		if (_ptr == _eptr)
- 			throw(std::invalid_argument("invalid stdex::stod argument"));
+			throw(std::invalid_argument("invalid stdex::stod argument"));
 		if(!_eptr)
 			throw(std::out_of_range("stdex::stod argument out of range"));
 
@@ -390,7 +591,7 @@ namespace stdex
 	{
 		const char *_eptr = s.c_str(), *_ptr = _eptr;
 		
-		typedef conditional<is_same<long double, double>::value, double, long double>::type type;
+		typedef conditional<sizeof(long double) == sizeof(double), double, long double>::type type;
 
 		long double _value = detail::_cs_to_floating_point<type>(_ptr, _eptr);
 
@@ -580,12 +781,45 @@ namespace stdex
 		string result(buf);
 
 		// some compilers ignore 'f' flag of spintf and print large values with scientific notation, as if 'e' flag was passed
-		// so we are removing substrings like 'e-10' and trying to enforce the precision:
+		// so we are removing substrings like 'e-10' and trying to enforce the precision by slow and not so precise conversion:
 
 		size_t e_pos = result.rfind('e'); 
 		if (e_pos != string::npos)
 		{
+#ifdef LDBL_MAX_10_EXP
+			char str_integer_part_reverse[LDBL_MAX_10_EXP + 3];
+#else
+			char str_integer_part_reverse[sizeof(buf)];
+#endif
+			long double fp_integer_part, fp_fractional_part;
+			size_t symbols_converted_n = 0;
+			fp_fractional_part = modf(value, &fp_integer_part);
+			while (fp_integer_part > 0.0L)
+			{
+				str_integer_part_reverse[symbols_converted_n++] = '0' + (int) fmod(fp_integer_part, 10.0L);
+				fp_integer_part = floor(fp_integer_part / 10.0L);
+			}
 
+			for (size_t i = 0; i < symbols_converted_n; i++)
+				buf[i] = str_integer_part_reverse[symbols_converted_n - i - 1];
+
+			buf[symbols_converted_n++] = '.';
+
+			if (fp_fractional_part > 0.0L)
+			{
+				while (fp_fractional_part > 0.0L)
+				{
+					fp_fractional_part *= 10.0L;
+					fp_fractional_part = modf(fp_fractional_part, &fp_integer_part);
+					buf[symbols_converted_n++] = '0' + (int) fp_integer_part;
+				}
+			}
+			else
+			   buf[symbols_converted_n++] = '0';
+
+			buf[symbols_converted_n] = 0; // EOS
+
+			result = buf;
 		}
 
 		return result;
