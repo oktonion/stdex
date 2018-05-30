a965daddfd1b72a65075cce41ff76cd799a3957a
Alexander <0oktonion0@gmail.com>
2018-05-31 00:45:46 +0300

    updated build script for library

--- stdex/build_lib.bat
+++ stdex/build_lib.bat
@@ -3,32 +3,38 @@ mkdir .\obj
 setlocal enabledelayedexpansion
 
 set build_ok=1
-set filename=""
 cd ..
-set INCLUDE=%INCLUDE%%cd%\pthread-win32\;
-cd ./stdex
+set INCLUDE=%INCLUDE%%cd%\pthread;
+
+echo "compiling %VisualStudioVersion% pthread-win32"
+cl -EHsc -Fo.\stdex\obj\pthread.obj -c ".\pthread-win32\pthread.c"
+if /I "%ERRORLEVEL%" NEQ "0" (
+    echo failed
+    return 1
+)
+
+cd .\stdex
 
 for /f %%f in ('dir /b ".\src\*.cpp"') do (
   echo "compiling %%~nf"
-  cl -EHsc -Fo.\obj\%%~nf.o -c ".\src\%%f"
+  cl -EHsc -Fo.\obj\%%~nf.obj -c ".\src\%%f"
   if /I "%ERRORLEVEL%" NEQ "0" (
     build_ok=0
   )
 )
 
-  if /I "%build_ok%" NEQ "1" (
-    echo "stdex.lib build failed"
-    return 1
-  )
-
-  mkdir .\lib
-
-  for /f %%f in ('dir /b ".\obj\*.o"') do (
-  echo %%f
-  echo "archiving %%~nf to stdex.lib"
-  lib -out:".\lib\stdex.lib" ".\obj\%%f"
+if /I "%build_ok%" NEQ "1" (
+  echo "stdex.lib build failed"
+  return 1
 )
+
+mkdir .\lib
+
+echo "archiving all obj-files to stdex.lib"
+lib -out:".\lib\stdex.lib" ".\obj\*.obj"
 
 dir .\lib\
 
 echo "lib done"
+
+::del /Q .\obj\*.obj
