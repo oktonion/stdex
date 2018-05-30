1676e6c75063ea32448c3070aa0c888a376fe686
Alexander <0oktonion0@gmail.com>
2018-05-31 01:03:12 +0300

    run tests script for windows

---
+++ run_tests.bat
@@ -0,0 +1,15 @@
+setlocal enabledelayedexpansion
+
+set run_ok=0
+for /f %%f in ('dir /b ".\tests\bin\*.exe"') do (
+  echo "running test %%~nf..."
+  .\tests\bin\%%f
+  if /I "%ERRORLEVEL%" NEQ "0" (
+    %run_ok%=-1
+    echo "...failed with %ERRORLEVEL%."
+  ) else (
+    echo "...ok."
+  )
+)
+
+return %run_ok%
