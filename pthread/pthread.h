22dac530c770a5e64cfcf1a38ae97cf9c04b2480
Alexander <0oktonion0@gmail.com>
2018-05-31 01:03:43 +0300

    custom pthread and shed headers for library

--- pthread/pthread.h
+++ pthread/pthread.h
@@ -1,4 +1,7 @@
 #define HAVE_CONFIG_H 
+
+#ifndef PTW32_STATIC_LIB
 #define PTW32_STATIC_LIB
+#endif
 
 #include "..\pthread-win32\pthread.h"
---
+++ pthread/sched.h
@@ -0,0 +1,6 @@
+#ifndef PTW32_STATIC_LIB
+#define PTW32_STATIC_LIB
+#endif
+
+
+#include "..\pthread-win32\sched.h"
