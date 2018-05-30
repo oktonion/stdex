392953afe447f948717036c2d907e41ff3283489
Alexander <0oktonion0@gmail.com>
2018-05-31 00:39:57 +0300

    bat for building tests

---
+++ build_tests.bat
@@ -0,0 +1,25 @@
+mkdir .\tests\bin
+mkdir .\tests\obj
+
+setlocal enabledelayedexpansion
+
+set build_ok=1
+set INCLUDE=%INCLUDE%%cd%\pthread\;
+
+for /f %%f in ('dir /b ".\tests\*.cpp"') do (
+  echo "compiling test %VisualStudioVersion% %%~nf"
+  cl -EHsc -Fo.\tests\obj\%%~nf.obj -c ".\tests\%%f"
+  if /I "%ERRORLEVEL%" NEQ "0" (
+    %build_ok%=0
+  )
+
+  if /I "%build_ok%" NEQ "0" (
+    cl .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
+  )
+)
+
+if /I "%build_ok%" NEQ "1" (
+  echo "tests build failed"
+)
+
+del /Q .\tests\obj\*.obj
