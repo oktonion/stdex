02bbca8c41ea27a2265132d920d56c6ae40a4010
oktonion <0oktonion0@gmail.com>
2018-08-01 17:57:21 +0300

    is_array for T[] (array with no size)

--- stdex/include/type_traits.hpp
+++ stdex/include/type_traits.hpp
@@ -693,7 +693,7 @@ namespace stdex
 			false_type
 		{ };
 
-		template<class _Tp, bool Choose = _is_incomplete_type<char[]>::value>
+		template<class _Tp, bool ImplCh = _is_incomplete_type<char[]>::value>
 		struct _is_array_impl:
 			_and_<_is_incomplete_type<_Tp>, _not_<is_function<_Tp> > >
 		{ };
