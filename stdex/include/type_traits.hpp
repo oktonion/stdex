62bc4d7f53c6fdd916238934492c99d70a5c2166
oktonion <0oktonion0@gmail.com>
2018-08-01 17:58:17 +0300

    merged from 'work' fixed

--- stdex/include/type_traits.hpp
+++ stdex/include/type_traits.hpp
@@ -695,6 +695,7 @@ namespace stdex
 
 		template<class _Tp, bool ImplCh = _is_incomplete_type<char[]>::value>
 		struct _is_array_impl:
+			_and_<_is_incomplete_type<_Tp>, _not_<is_function<_Tp> > >
 		{ };
 
 		template<class _Tp>
