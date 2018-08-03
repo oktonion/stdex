44d755018ac25c32a08f63de2b701e089d4c8ac7
oktonion <0oktonion0@gmail.com>
2018-08-03 13:32:45 +0300

    correct ERRORLEVEL for win build (I hope)

--- stdex/build_lib.bat
+++ stdex/build_lib.bat
@@ -15,8 +15,8 @@ if ["%~1"]==["debug"] (
 
 echo "compiling %VisualStudioVersion% pthread-win32"
 cl -EHsc -W4 -Fo.\stdex\obj\pthread.obj -D HAVE_CONFIG_H -c ".\pthread-win32\pthread.c" %build_opt%
-if /I "%ERRORLEVEL%" NEQ "0" (
-    set "build_ok=0"
+if ERRORLEVEL 1 set "build_ok=0"
+if /I "%build_ok%" NEQ "1" (
     echo "failed"
     exit /B 1
 )
@@ -26,9 +26,7 @@ cd .\stdex
 for /f %%f in ('dir /b ".\src\*.cpp"') do (
   echo "compiling %%~nf"
   cl -EHsc -W4 -Fo.\obj\%%~nf.obj -c ".\src\%%f" %build_opt%
-  if /I "%ERRORLEVEL%" NEQ "0" (
-    set "build_ok=0"
-  )
+  if ERRORLEVEL 1 set "build_ok=0"
 )
 
 if /I "%build_ok%" NEQ "1" (
