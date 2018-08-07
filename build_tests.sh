dd8660bb35c6f47165d50bcb11a5263d4d2d80c9
Alexander <0oktonion0@gmail.com>
2018-08-07 23:16:04 +0300

    build script for unix is fixed

--- build_tests.sh
+++ build_tests.sh
@@ -9,14 +9,18 @@ else
   exclude_warn="-Wno-long-long"
 fi
 
-for file in ./tests/*.cpp; do
-  filename=$(basename -- "$file")
-  filename="${filename%.*}"
-  echo "compiling test c++03 $filename"
-  if ! $COMPILER -std=c++03 -pedantic $exclude_warn $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
-    build_ok=0
-  fi
-done
+if [[ $COMPILER = *"g++-4."* ]]; then
+  echo "c++03 option is not supported"
+else
+  for file in ./tests/*.cpp; do
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
+    echo "compiling test c++03 $filename"
+    if ! $COMPILER -std=c++03 -pedantic $exclude_warn $file -L./stdex/lib/ -lstdex -lrt -lpthread -o "./tests/bin/$filename"; then
+      build_ok=0
+    fi
+  done
+fi
 
 if [ $build_ok -eq 0 ]; then
   echo "tests build failed"
