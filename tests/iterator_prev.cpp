acca29ab56b1d81ab05bc6dc338fa0b289e36a6c
oktonion <0oktonion0@gmail.com>
2018-10-23 17:44:14 +0300

    + iterator prev test

---
+++ tests/iterator_prev.cpp
@@ -0,0 +1,36 @@
+#include "../stdex/include/core.h"
+#include "../stdex/include/iterator.hpp"
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+#define DYNAMIC_VERIFY(cond) if(!(cond)) {std::cout << "check condition \'" << #cond << "\' failed at line " << __LINE__ << std::endl; return __LINE__;}
+#define RUN_TEST(test) {std::cout << #test << std::endl; int line = test(); if(line != 0) {std::cout << "failed at line " << line << std::endl; return line;}}
+
+int test01()
+{
+  std::vector<int> c(3);    
+  std::vector<int>::iterator i = c.end(), j;
+
+  j = stdex::prev(i, 3);
+  DYNAMIC_VERIFY( i == c.end() );
+  DYNAMIC_VERIFY( j == c.begin() );
+}
+
+int test02()
+{
+  std::list<int> c(3);    
+  std::list<int>::iterator i = c.end(), j;
+
+  j = stdex::prev(i, 3);
+  DYNAMIC_VERIFY( i == c.end() );
+  DYNAMIC_VERIFY( j == c.begin() );
+}
+
+int main()
+{
+  RUN_TEST(test01);
+  RUN_TEST(test02);
+  return 0;
+}
