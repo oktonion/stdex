2488ddf107d4bdd7dd504bdfd339cecf120d73d1
oktonion <0oktonion0@gmail.com>
2018-10-29 17:30:07 +0300

    iterator header is pretty and standard
    TODO: make_reverse_iterator

--- stdex/include/iterator.hpp
+++ stdex/include/iterator.hpp
@@ -27,47 +27,62 @@
 
 namespace stdex
 {
-	template<class ContainerType>
-	inline
-	typename ContainerType::iterator begin(ContainerType &value)
-	{	// get beginning of sequence
-		return (value.begin());
-	}
+	// Iterator primitives 
 
-	template<class ContainerType>
-	inline
-	typename ContainerType::iterator begin(const ContainerType &value)
-	{	// get beginning of sequence
-		return (value.begin());
-	}
+	using std::iterator_traits; // provides uniform interface to the properties of an iterator
+								// (class template)
+	using std::input_iterator_tag;
+	using std::output_iterator_tag;
+	using std::forward_iterator_tag;
+	using std::bidirectional_iterator_tag;
+	using std::random_access_iterator_tag;
 
-	template<class ContainerType>
-	inline
-	typename ContainerType::iterator end(ContainerType &value)
-	{	// get end of sequence
-		return (value.end());
-	}
+	using std::iterator; // base class to ease the definition of required types for simple iterators
+						 // (class template)
 
-	template<class ContainerType>
-	inline
-	typename ContainerType::iterator end(const ContainerType &value)
-	{	// get end of sequence
-		return (value.end());
-	}
+	//Iterator adaptors
 
-	template<class T, std::size_t Size>
-	inline
-	T *begin(T(&value)[Size]) NOEXCEPT_FUNCTION
-	{	// get beginning of array
-		return (value);
-	}
+	using std::reverse_iterator; // iterator adaptor for reverse-order traversal
+								 // (class template)
+	//<TODO>: using std::make_reverse_iterator; // (C++14)
+									  // creates a std::reverse_iterator of type inferred from the argument
+									  // (function template)
+	//using std::move_iterator; // (C++11)
+							  // iterator adaptor which dereferences to an rvalue reference
+							  // (class template)
+	//using std::make_move_iterator; 	// (C++11)
+									// creates a std::move_iterator of type inferred from the argument
+									// (function template)
+	using std::back_insert_iterator; // iterator adaptor for insertion at the end of a container
+									 // (class template)
+	using std::back_inserter; // creates a std::back_insert_iterator of type inferred from the argument
+							  // (function template)
+	using std::front_insert_iterator; // iterator adaptor for insertion at the front of a container
+									  // (class template)
+	using std::front_inserter; // creates a std::front_insert_iterator of type inferred from the argument
+							   // (function template)
+	using std::insert_iterator; 	// iterator adaptor for insertion into a container
+									// (class template)
+	using std::inserter; // creates a std::insert_iterator of type inferred from the argument
+						 // (function template)
 
-	template<class T, std::size_t Size>
-	inline
-	T *end(T(&value)[Size]) NOEXCEPT_FUNCTION
-	{	// get end of array
-		return (value + Size);
-	}
+	// Stream iterators
+
+	using std::istream_iterator; // input iterator that reads from std::basic_istream
+								 // (class template)
+	using std::ostream_iterator; // output iterator that writes to std::basic_ostream
+								 // (class template)
+	using std::istreambuf_iterator; // input iterator that reads from std::basic_streambuf
+									// (class template)
+	using std::ostreambuf_iterator; // output iterator that writes to std::basic_streambuf
+									// (class template)
+
+	// Iterator operations
+
+	using std::advance; // advances an iterator by given distance
+						// (function)
+	using std::distance; // returns the distance between two iterators
+						 // (function)
 
 	namespace detail
 	{
@@ -151,7 +166,7 @@ namespace stdex
 	typename 
 		detail::_iterator_cat_is_forward<ForwardIt>::
 	type next(ForwardIt it,
-		typename std::iterator_traits<ForwardIt>::difference_type n = 1)
+		typename std::iterator_traits<ForwardIt>::difference_type n = 1) // increment an iterator 
 	{
 		std::advance(it, n);
 		return it;
@@ -162,11 +177,61 @@ namespace stdex
 	typename 
 		detail::_iterator_cat_is_bi<BidirIt>::
 	type prev(BidirIt it,
-	typename std::iterator_traits<BidirIt>::difference_type n = 1)
+	typename std::iterator_traits<BidirIt>::difference_type n = 1) // decrement an iterator 
 	{
 		std::advance(it, -n);
 		return it;
 	}
+
+	// Range access 
+
+	// begin (C++11)
+	// returns an iterator to the beginning of a container or array 
+	template<class ContainerType>
+	inline
+	typename ContainerType::iterator begin(ContainerType &value)
+	{	// get beginning of sequence
+		return (value.begin());
+	}
+	
+	template<class ContainerType>
+	inline
+	typename ContainerType::iterator begin(const ContainerType &value)
+	{	// get beginning of sequence
+		return (value.begin());
+	}
+
+	template<class T, std::size_t Size>
+	inline
+	T *begin(T(&value)[Size]) NOEXCEPT_FUNCTION
+	{	// get beginning of array
+		return (value);
+	}
+
+	// end (C++11)
+	// returns an iterator to the end of a container or array 
+	template<class ContainerType>
+	inline
+	typename ContainerType::iterator end(ContainerType &value)
+	{	// get end of sequence
+		return (value.end());
+	}
+
+	template<class ContainerType>
+	inline
+	typename ContainerType::iterator end(const ContainerType &value)
+	{	// get end of sequence
+		return (value.end());
+	}
+
+	template<class T, std::size_t Size>
+	inline
+	T *end(T(&value)[Size]) NOEXCEPT_FUNCTION
+	{	// get end of array
+		return (value + Size);
+	}
+
+	// Container access (C++ 17)
 }
 
 #endif // _STDEX_ITERATOR_H
