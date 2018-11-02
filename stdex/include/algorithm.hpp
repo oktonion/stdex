ae5e79b2c963196eb01f5c684485f1eedaba1d9e
oktonion <0oktonion0@gmail.com>
2018-11-02 14:01:10 +0300

    + all algorithms

--- stdex/include/algorithm.hpp
+++ stdex/include/algorithm.hpp
@@ -14,6 +14,8 @@
 #include <algorithm>
 #include <cstddef> // std::size_t
 #include <cassert> // assert
+#include <cstdlib> // std::rand
+#include <map> // std::pair
 
 #ifdef _STDEX_NATIVE_CPP11_SUPPORT
 
@@ -69,21 +71,35 @@ namespace stdex
 		return true;
 	}
 
-	using std::for_each; // applies a function to a range of elements
-						 // (function template)
+	// applies a function to a range of elements
+	// (function template)
+	using std::for_each; 
+						 
 	//<TODO>: using std::for_each_n; // (C++17) applies a function object to the first n elements of a sequence
 					  // (function template)
-	using std::count; // returns the number of elements
-	using std::count_if; // returns the number of elements satisfying specific criteria
-						 // (function template)
-	using std::mismatch; // finds the first position where two ranges differ
-						 // (function template)
-	using std::equal; // determines if two sets of elements are the same
-					  // (function template)
+
+	// returns the number of elements					  
+	using std::count; 
+	
+	// returns the number of elements satisfying specific criteria
+	// (function template)
+	using std::count_if; 
+
+	// finds the first position where two ranges differ						 
+	// (function template)
+	using std::mismatch; 
+
+	// determines if two sets of elements are the same						 
+	// (function template)
+	using std::equal; 
+					  
 
 	// finds the first element satisfying specific criteria                  
 	using std::find;
+
+	// finds the first element satisfying specific criteria   
 	using std::find_if; 
+
 	// find_if_not (C++11)
 	template<class _InputIt, class _UnaryPredicate>
 	inline 
@@ -98,17 +114,26 @@ namespace stdex
 		return last;
 	}
 
-	using std::find_end; // finds the last sequence of elements in a certain range
-						 // (function template)
-	using std::find_first_of; // searches for any one of a set of elements
-							  // (function template)
-	using std::adjacent_find; // finds the first two adjacent items that are equal (or satisfy a given predicate)
-							  // (function template)
-	using std::search; // searches for a range of elements
-					   // (function template)
-	using std::search_n; // searches a range for a number of consecutive copies of an element
-						 // (function template)
+	// finds the last sequence of elements in a certain range
+	// (function template)
+	using std::find_end; 
 
+	// searches for any one of a set of elements
+	// (function template)						 
+	using std::find_first_of; 
+
+	// finds the first two adjacent items that are equal (or satisfy a given predicate)
+	// (function template)							  
+	using std::adjacent_find; 
+
+	// searches for a range of elements
+	// (function template)							  
+	using std::search; 
+
+	// searches a range for a number of consecutive copies of an element
+	// (function template)					   
+	using std::search_n; 
+						 
 	// Modifying sequence operations 
 
 	// copies a range of elements to a new location
@@ -118,8 +143,11 @@ namespace stdex
 	inline
 	typename 
 		detail::_iterator_is_valid_output<_OutputIt>::
-	type  copy_if(_InputIt first, 
-		typename detail::_iterator_cat_is_input<_InputIt>::type last, _OutputIt d_first, _UnaryPredicate p)
+	type  copy_if(
+		_InputIt first, 
+		typename detail::_iterator_cat_is_input<_InputIt>::type last, 
+		_OutputIt d_first, 
+		_UnaryPredicate p)
 	{
 		while (first != last) {
 			if (p(*first))
@@ -221,7 +249,11 @@ namespace stdex
 		return result;
 	}
 
-	template<class _InputT, std::size_t _InputSize, class _Diff, class _OutputT, std::size_t _OutputSize>
+	template<
+		class _InputT, std::size_t _InputSize, 
+		class _Diff, 
+		class _OutputT, std::size_t _OutputSize
+		>
 	inline
 	typename 
 		detail::_copy_n_input_it_check<_InputT*, _OutputT>::
@@ -241,6 +273,512 @@ namespace stdex
 		}
 		return result;
 	}
+
+	// copies a range of elements in backwards order
+	// (function template)
+	using std::copy_backward;
+
+	// (C++11)
+	// moves a range of elements to a new location
+	// (function template)
+	// <TODO>: move()
+
+	// (C++11)
+	// moves a range of elements to a new location in backwards order
+	// (function template)
+	// <TODO>: move_backward()
+
+	// copy-assigns the given value to every element in a range
+	// (function template)
+	using std::fill;
+
+	// copy-assigns the given value to N elements in a range
+	// (function template)
+	using std::fill_n;
+
+	// applies a function to a range of elements
+	// (function template)
+	using std::transform;
+
+	// assigns the results of successive function calls to every element in a range
+	// (function template)
+	using std::generate;
+
+	// assigns the results of successive function calls to N elements in a range
+	// (function template)
+	using std::generate_n;
+
+	// removes elements satisfying specific criteria
+	// (function template)
+	using std::remove;
+
+	// removes elements satisfying specific criteria
+	// (function template)
+	using std::remove_if;
+
+	// copies a range of elements omitting those that satisfy specific criteria
+	// (function template)
+	using std::remove_copy;
+
+	// copies a range of elements omitting those that satisfy specific criteria
+	// (function template)
+	using std::remove_copy_if;
+
+	// replaces all values satisfying specific criteria with another value
+	// (function template)
+	using std::replace;
+
+	// replaces all values satisfying specific criteria with another value
+	// (function template)
+	using std::replace_if;
+
+	// copies a range, replacing elements satisfying specific criteria with another value
+	// (function template)
+	using std::replace_copy;
+
+	// copies a range, replacing elements satisfying specific criteria with another value
+	// (function template)
+	using std::replace_copy_if;
+
+	// swaps the values of two objects
+	// (function template)
+	using std::swap;
+
+	// swaps two ranges of elements
+	// (function template)
+	using std::swap_ranges;
+
+	// swaps the elements pointed to by two iterators
+	// (function template)
+	using std::iter_swap;
+
+	// reverses the order of elements in a range
+	// (function template)
+	using std::reverse;
+
+	// creates a copy of a range that is reversed
+	// (function template)
+	using std::reverse_copy;
+
+	// rotates the order of elements in a range
+	// (function template)
+	using std::rotate;
+
+	// copies and rotate a range of elements
+	// (function template)
+	using std::rotate_copy;
+
+	// randomly re-orders elements in a range
+	// (function template)
+	template<class _RandomIt>
+	inline
+	void random_shuffle(_RandomIt first, 
+		typename detail::_iterator_cat_is_rand_access<_RandomIt>::type last)
+	{
+		typename std::iterator_traits<_RandomIt>::difference_type i, n;
+		n = last - first;
+		for (i = n-1; i > 0; --i) {
+			swap(first[i], first[std::rand() % (i+1)]);
+			// rand() % (i+1) isn't actually correct, because the generated number
+			// is not uniformly distributed for most values of i. A correct implementation
+			// will need to essentially reimplement C++11 std::uniform_int_distribution,
+			// which is not implemented (yet).
+		}
+	}
+
+	// randomly re-orders elements in a range with user random number generator func
+	// (function template)
+	template<class _RandomIt, class _RandomFunc>
+	inline
+	void random_shuffle(_RandomIt first,
+		typename detail::_iterator_cat_is_rand_access<_RandomIt>::type last, _RandomFunc &r)
+	{
+		typename std::iterator_traits<RandomIt>::difference_type i, n;
+		n = last - first;
+		for (i = n-1; i > 0; --i) {
+			swap(first[i], first[r(i+1)]);
+		}
+	}
+	// (C++11)
+	// randomly re-orders elements in a range
+	// (function template)
+	// <TODO>: shuffle()
+
+	// removes consecutive duplicate elements in a range
+	// (function template)
+	using std::unique;
+
+	// creates a copy of some range of elements that contains no consecutive duplicates
+	// (function template)
+	using std::unique_copy;
+
+	// Partitioning operations
+
+	// (C++11)
+	// determines if the range is partitioned by the given predicate
+	// (function template)
+	template<class _InputIt, class _UnaryPredicate>
+	inline
+	bool is_partitioned(_InputIt first, 
+		typename detail::_iterator_cat_is_input<_InputIt>::type last, _UnaryPredicate p)
+	{
+		for (; first != last; ++first)
+			if (!p(*first))
+				break;
+		for (; first != last; ++first)
+			if (p(*first))
+				return false;
+		return true;
+	}
+
+	// divides a range of elements into two groups
+	// (function template)
+	using std::partition;
+
+	// (C++11)
+	// copies a range dividing the elements into two groups
+	// (function template)
+	template<class _InputIt, class _OutputIt1, class _OutputIt2, class _UnaryPredicate>
+	inline
+	std::pair<_OutputIt1, _OutputIt2>
+		partition_copy(
+			_InputIt first, 
+			typename detail::_iterator_cat_is_input<_InputIt>::type last, 
+			_OutputIt1 d_first_true, 
+			typename detail::_iterator_is_valid_output<_OutputIt2>::type d_first_false,
+		_UnaryPredicate p)
+	{
+		while (first != last) {
+			if (p(*first)) {
+				*d_first_true = *first;
+				++d_first_true;
+			} else {
+				*d_first_false = *first;
+				++d_first_false;
+			}
+			++first;
+		}
+		return std::pair<OutputIt1, OutputIt2>(d_first_true, d_first_false);
+	}
+
+	// divides elements into two groups while preserving their relative order
+	// (function template)
+	using std::stable_partition;
+	
+	// (C++11)
+	// locates the partition point of a partitioned range
+	// (function template)
+	// <TODO>: partition_point()
+
+
+	// Sorting operations
+
+	// (C++11)
+	// checks whether a range is sorted into ascending order
+	// (function template)
+	template<class _ForwardIt>
+	inline
+	bool is_sorted(_ForwardIt first, 
+		typename detail::_iterator_cat_is_forward<_ForwardIt>::type last)
+	{
+		return std::is_sorted_until(first, last) == last;
+	}
+
+	// (C++11)
+	// checks with given binary comparison function comp whether a range is sorted into ascending order
+	// (function template)
+	template<class _ForwardIt, class _Compare>
+	inline
+	bool is_sorted(_ForwardIt first, 
+		typename detail::_iterator_cat_is_forward<_ForwardIt>::type last, _Compare comp)
+	{
+		return std::is_sorted_until(first, last, comp) == last;
+	}
+
+	// (C++11)
+	// finds the largest sorted subrange
+	// (function template)
+	template<class _ForwardIt>
+	inline
+	_ForwardIt is_sorted_until(_ForwardIt first, 
+		typename detail::_iterator_cat_is_forward<_ForwardIt>::type last)
+	{
+		if (first != last) {
+			ForwardIt next = first;
+			while (++next != last) {
+				if (*next < *first)
+					return next;
+				first = next;
+			}
+		}
+		return last;
+	}
+
+	// (C++11)
+	// finds the largest sorted subrange given binary comparison function comp
+	// (function template)
+	template <class _ForwardIt, class _Compare>
+	inline
+	_ForwardIt is_sorted_until(_ForwardIt first, 
+		typename detail::_iterator_cat_is_forward<_ForwardIt>::type last, _Compare comp) 
+	{
+		if (first != last) {
+			ForwardIt next = first;
+			while (++next != last) {
+				if (true == comp(*next, *first))
+					return next;
+				first = next;
+			}
+		}
+		return last;
+	}
+
+	// sorts a range into ascending order
+	// (function template)
+	using std::sort;
+
+	// sorts the first N elements of a range
+	// (function template)
+	using std::partial_sort;
+
+	// copies and partially sorts a range of elements
+	// (function template)
+	using std::partial_sort_copy;
+
+	// sorts a range of elements while preserving order between equal elements
+	// (function template)
+	using std::stable_sort;
+
+	// partially sorts the given range making sure that it is partitioned by the given element
+	// (function template)	
+	using std::nth_element;
+
+	// Binary search operations (on sorted ranges)
+
+	// returns an iterator to the first element not less than the given value
+	// (function template)
+	using std::lower_bound;
+
+	// returns an iterator to the first element greater than a certain value
+	// (function template)
+	using std::upper_bound;
+
+	// determines if an element exists in a certain range
+	// (function template)
+	using std::binary_search;
+
+	// returns range of elements matching a specific key
+	// (function template)
+	using std::equal_range;
+
+	// Set operations (on sorted ranges)
+
+	// merges two sorted ranges
+	// (function template)
+	using std::merge;
+
+	// merges two ordered ranges in-place
+	// (function template)
+	using std::inplace_merge;
+
+	// returns true if one set is a subset of another
+	// (function template)
+	using std::includes;
+
+	// computes the difference between two sets
+	// (function template)
+	using std::set_difference;
+
+	// computes the intersection of two sets
+	// (function template)
+	using std::set_intersection;
+
+	// computes the symmetric difference between two sets
+	// (function template)
+	using std::set_symmetric_difference;
+
+	// computes the union of two sets
+	// (function template)
+	using std::set_union;
+
+	// Heap operations
+
+	// (C++11)
+	// checks if the given range is a max heap
+	// (function template)
+	// <TODO>: is_heap()
+
+	// (C++11)
+	// finds the largest subrange that is a max heap
+	// (function template)
+	// <TODO>: is_heap_until()
+
+	// creates a max heap out of a range of elements
+	// (function template)
+	using std::make_heap;
+
+	// adds an element to a max heap
+	// (function template)
+	using std::push_heap;
+
+	// removes the largest element from a max heap
+	// (function template)
+	using std::pop_heap;
+
+	// turns a max heap into a range of elements sorted in ascending order
+	// (function template)
+	using std::sort_heap;
+
+	// Minimum/maximum operations
+
+	// (C++17)
+	// clamps a value between a pair of boundary values
+	// (function template)
+	// <TODO>: clamp()
+
+	// returns the greater of the given values
+	// (function template)
+	using std::max;
+
+	// returns the largest element in a range
+	// (function template)
+	using std::max_element;
+
+	// returns the smaller of the given values
+	// (function template)
+	using std::min;
+
+	// returns the smallest element in a range
+	// (function template)
+	using std::min_element;
+
+	// (C++11)
+	// returns the smaller and larger of two elements
+	// (function template)
+	template<class _Tp> 
+	inline
+	std::pair<const _Tp&, const _Tp&> minmax( const _Tp& a, const _Tp& b )
+	{
+		return (b < a) ? std::pair<const _Tp&, const _Tp&>(b, a)
+					: std::pair<const _Tp&, const _Tp&>(a, b);
+	}
+
+	// (C++11)
+	// returns the smaller and larger of two elements using the given comparison function comp
+	// (function template)
+	template<class _Tp, class _Compare> 
+	inline
+	std::pair<const _Tp&, const _Tp&> minmax( const _Tp& a, const _Tp& b, _Compare comp )
+	{
+		return comp(b, a) ? std::pair<const _Tp&, const _Tp&>(b, a)
+						: std::pair<const _Tp&, const _Tp&>(a, b);
+	}
+
+	// (C++11)
+	// returns the smallest and the largest elements in a range 
+	// using the given binary comparison function comp
+	template<class _ForwardIt, class _Compare>
+	inline
+	std::pair<_ForwardIt, _ForwardIt> 
+		minmax_element(_ForwardIt first, 
+			typename detail::_iterator_cat_is_forward<_ForwardIt>::type last, _Compare comp)
+	{
+		std::pair<_ForwardIt, _ForwardIt> result(first, first);
+	
+		if (first == last) return result;
+		if (++first == last) return result;
+	
+		if (comp(*first, *result.first)) {
+			result.first = first;
+		} else {
+			result.second = first;
+		}
+		while (++first != last) {
+			_ForwardIt i = first;
+			if (++first == last) {
+				if (comp(*i, *result.first)) result.first = i;
+				else if (!(comp(*i, *result.second))) result.second = i;
+				break;
+			} else {
+				if (comp(*first, *i)) {
+					if (comp(*first, *result.first)) result.first = first;
+					if (!(comp(*i, *result.second))) result.second = i;
+				} else {
+					if (comp(*i, *result.first)) result.first = i;
+					if (!(comp(*first, *result.second))) result.second = first;
+				}
+			}
+		}
+		return result;
+	}
+
+	// (C++11)
+	// returns the smallest and the largest elements in a range
+	// (function template)
+	template<class _ForwardIt>
+	inline
+	std::pair<_ForwardIt, _ForwardIt> 
+		minmax_element(_ForwardIt first, 
+			typename detail::_iterator_cat_is_forward<_ForwardIt>::type last)
+	{
+		return std::minmax_element(first, last, std::less<>());
+	}
+
+	// returns true if one range is lexicographically less than another
+	// (function template)
+	using std::lexicographical_compare;
+
+	namespace detail
+	{
+		template<class _ForwardIt2>
+		struct _is_permutation_arg3_check:
+			_iterator_enable_if<
+				_iterator_cat_is<
+					typename std::iterator_traits<_ForwardIt2>::iterator_category,
+					std::forward_iterator_tag
+					>::value == bool(true) 
+				bool
+			>
+		{ };
+	}
+
+	// (C++11)
+	// determines if a sequence is a permutation of another sequence
+	// (function template)
+	template<class _ForwardIt1, class _ForwardIt2>
+	inline
+	typename 
+		detail::_is_permutation_arg3_check<_ForwardIt2>::
+	type is_permutation(_ForwardIt1 first, 
+		typename detail::_iterator_cat_is_forward<_ForwardIt1>::type last, _ForwardIt2 d_first)
+	{
+		// skip common prefix
+		std::pair<_ForwardIt1, _ForwardIt2> tie = std::mismatch(first, last, d_first);
+		first = tie.first;
+		d_first = tie.second;
+		// iterate over the rest, counting how many times each element
+		// from [first, last) appears in [d_first, d_last)
+		if (first != last) {
+			_ForwardIt2 d_last = d_first;
+			std::advance(d_last, std::distance(first, last));
+			for (_ForwardIt1 i = first; i != last; ++i) {
+					if (i != std::find(first, i, *i)) continue; // already counted this *i
+					typename iterator_traits<_ForwardIt2>::difference_type m = std::count(d_first, d_last, *i);
+					if (m==0 || std::count(i, last, *i) != m) {
+						return false;
+					}
+				}
+			}
+		return true;
+	}
+
+	// generates the next greater lexicographic permutation of a range of elements
+	// (function template)
+	using std::next_permutation;
+
+	// generates the next smaller lexicographic permutation of a range of elements
+	// (function template)
+	using std::prev_permutation;
 }
 
 #endif // _STDEX_ALGORITHM_H
