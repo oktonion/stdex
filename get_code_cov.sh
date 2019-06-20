20c46da00800d82fac62a3958292e13b042157b8
Alexander <0oktonion0@gmail.com>
2019-06-20 19:28:10 +0300

    Update get_code_cov.sh

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-mkdir ./tests/coverage
-for filename in `find ./tests/ | egrep '\.cpp'`; 
-do 
-  gcov -n -o ./tests/bin/ $filename > /dev/null; 
+#for filename in `find ./tests/ | egrep '\.cpp'`; 
+#do 
+ # gcov -n -o ./tests/bin/ $filename > /dev/null; 
+#done
+
+for file in ./tests/bin/*; do
+    gcov file.gcda
 done
