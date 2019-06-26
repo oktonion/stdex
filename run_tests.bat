f61226c363925734de84d693b08296da07574eb9
Alexander <0oktonion0@gmail.com>
2019-06-27 02:32:59 +0300

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,20 +1,22 @@
 setlocal enabledelayedexpansion
 
-set "run_ok=true"
+set "run_ok=1"
 for /f %%f in ('dir /b ".\tests\bin\*.exe"') do (
   echo "running test %%~nf..."
   
   .\tests\bin\%%f --duration
 
   IF ERRORLEVEL 1 (
-    set "run_ok=false"
+    set "run_ok=0"
     echo "...failed with %ERRORLEVEL%."
   ) else (
     echo "...ok."
   )
 )
 
-if "%run_ok%" == "false" (
+if /I "%run_ok%" NEQ "1" (
   echo "tests run failed"
   exit /B 1
 )
+
+echo "tests run ok"
