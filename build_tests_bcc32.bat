751c62f9f0225ab3947cf920b3adecb220e1f522
Alexander <0oktonion0@gmail.com>
2019-06-27 01:18:26 +0300

    Update build_tests_bcc32.bat

--- build_tests_bcc32.bat
+++ build_tests_bcc32.bat
@@ -7,15 +7,16 @@ set "build_ok=true"
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test Borland C++ %%~nf"
-  bcc32 -w -Q -n.\tests\obj\ -L.\stdex\lib\ -I%cd%\pthread\  -c".\tests\%%f"
+  bcc32 -w-inl -w-ccc -tWM -Q -n.\tests\obj\ -L.\stdex\lib\ -I%cd%\pthread\ -c ".\tests\%%f"
   IF ERRORLEVEL 1 (
     set "build_ok=false"
   )
 
   if /I "%build_ok%" NEQ "0" (
-    bcc32 -w -Q -L.\stdex\lib\ -I%cd%\pthread\ stdex.lib -n.\tests\bin\ .\tests\obj\%%~nf.obj
+    bcc32 -w-inl -w-ccc -tWM -Q -L.\stdex\lib\ -lap -I%cd%\pthread\ stdex.lib cw32mt.lib ntdll.lib -o.\tests\bin\%%~nf.exe .\tests\obj\%%~nf.obj
     IF ERRORLEVEL 1 (
       set "build_ok=false"
+      exit /B 1
     )
   )
 )
