7321c2b21a920a8dc40f4c66ed3e4a8ea6c3dd36
Alexander <0oktonion0@gmail.com>
2019-06-27 02:18:42 +0300

    win build scripts updated

--- build_tests.bat
+++ build_tests.bat
@@ -3,27 +3,29 @@ mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
 
-set "build_ok=true"
+set "build_ok=1"
 set INCLUDE=%INCLUDE%;%cd%\pthread\;
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test %VisualStudioVersion% %%~nf"
   cl -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -c ".\tests\%%f"
   IF ERRORLEVEL 1 (
-    set "build_ok=false"
+    set "build_ok=0"
   )
 
-  if /I "%build_ok%" NEQ "0" (
+  if "%build_ok%"=="1" (
     cl /I %cd%\pthread\ .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
     IF ERRORLEVEL 1 (
-      set "build_ok=false"
+      set "build_ok=0"
     )
   )
 )
 
-if "%build_ok%" == "false" (
+if /I "%build_ok%" NEQ "1" (
   echo "tests build failed"
   exit /B 1
 )
+
+echo "tests build ok"
 
 del /Q .\tests\obj\*.obj
--- build_tests_bcc32.bat
+++ build_tests_bcc32.bat
@@ -3,27 +3,29 @@ mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
 
-set "build_ok=true"
+set "build_ok=1"
+
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test Borland C++ %%~nf"
   bcc32 -w-inl -w-ccc -tWM -Q -n.\tests\obj\ -L.\stdex\lib\ -I%cd%\pthread\ -c ".\tests\%%f"
   IF ERRORLEVEL 1 (
-    set "build_ok=false"
+    set "build_ok=0"
   )
 
-  if /I "%build_ok%" NEQ "0" (
+  if "%build_ok%"=="1" (
     bcc32 -w-inl -w-ccc -tWM -Q -L.\stdex\lib\ -lap -I%cd%\pthread\ stdex.lib cw32mt.lib ntdll.lib -o.\tests\bin\%%~nf.exe .\tests\obj\%%~nf.obj
     IF ERRORLEVEL 1 (
-      set "build_ok=false"
-      exit /B 1
+      set "build_ok=0"
     )
   )
 )
 
-if "%build_ok%" == "false" (
+if /I "%build_ok%" NEQ "1" (
   echo "tests build failed"
   exit /B 1
 )
+
+echo "tests build ok"
 
 del /Q .\tests\obj\*.obj
