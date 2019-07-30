5721c1ff306b0570878557f16c09523a97678ee6
oktonion <0oktonion0@gmail.com>
2019-07-30 10:32:58 +0300

    moved type traits for tests to type_traits namespace

--- tests/find_if_not.cpp
+++ tests/find_if_not.cpp
@@ -19,14 +19,17 @@ template<class T>
     { }
 };
 
-template<class T> struct remove_cv { typedef T type; };
-template<class T> struct remove_cv<const T> { typedef T type; };
-template<class T> struct remove_cv<volatile T> { typedef T type; };
-template<class T> struct remove_cv<const volatile T> { typedef T type; };
+namespace type_traits
+{
+	template<class T> struct remove_cv { typedef T type; };
+	template<class T> struct remove_cv<const T> { typedef T type; };
+	template<class T> struct remove_cv<volatile T> { typedef T type; };
+	template<class T> struct remove_cv<const volatile T> { typedef T type; };
+}
 
 template<class T>
 class input_iterator_wrapper
-: public std::iterator<std::input_iterator_tag, typename ::remove_cv<T>::type,
+: public std::iterator<std::input_iterator_tag, typename type_traits::remove_cv<T>::type,
       std::ptrdiff_t, T*, T&>
 {
 protected:
