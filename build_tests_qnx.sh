73899d70f60072afdbc63e69c02a3c37c6fb54e6
Alexander <oktonion.work@gmail.com>
2019-12-12 12:56:44 +0300

    Update build_tests_qnx.sh
    cpp98 build

--- build_tests_qnx.sh
+++ build_tests_qnx.sh
@@ -25,3 +25,22 @@ if [ $build_ok -eq 0 ]; then
   echo "$tests_failed"
   exit 3
 fi
+
+if [[ $COMPILER != *"qcc"* ]]; then
+
+for file in ./tests/*.cpp; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "compiling test c++98 $filename"
+  if ! $COMPILER -std=c++98 -pedantic $exclude_warn $file -L./stdex/lib/ -lstdex -lm -o "./tests/bin/$filename"; then
+    build_ok=0
+    tests_failed="$tests_failed $filename;"
+  fi
+done
+
+fi
+
+if [ $build_ok -eq 0 ]; then
+  echo "$tests_failed"
+  exit 98
+fi
