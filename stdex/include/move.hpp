01027dd66ac1b5e6c26d9264e5f5f8ad86ca29e9
oktonion <0oktonion0@gmail.com>
2020-03-05 11:08:30 +0300

    Update move.hpp

--- stdex/include/move.hpp
+++ stdex/include/move.hpp
@@ -58,21 +58,18 @@ namespace stdex
     class rvalue_ref:
 		public remove_const<T>::type
     {
-		delete_implicit_copy_constructor icc_deleter;
-		typedef typename remove_const<T>::type type;
 	protected:
+		typedef typename remove_const<T>::type type;
+		const type& _ref;
+
         explicit rvalue_ref(const type &ref) 
 			: type(ref),
-			icc_deleter(true),
-            _ref(const_cast<type&>(ref)) 
+            _ref((ref)) 
 		{ }
-		
-        type& _ref;
     public:
 		template<class TT>
         rvalue_ref(rvalue_ref<TT> &other)
 			: type(other._ref),
-			icc_deleter(true),
 			_ref(other._ref) 
 		{ }
         //T& get() const {return _ref;}
@@ -83,35 +80,26 @@ namespace stdex
 		{
 			return rvalue_ref<type>(value);
 		}
-		static rvalue_ref<type> move(const type &value) 
+		static rvalue_ref<const type> move(const type &value) 
 		{
-			return rvalue_ref<type>(const_cast<type&>(value));
+			return rvalue_ref<const type>(value);
 		}
 		static rvalue_ref<type> move(const rvalue_ref<type>& value)
 		{
-			return rvalue_ref<type>::move(value._ref);
+			return rvalue_ref<type>(value._ref);
 		}
     };
 
-	template<class T>
-	struct ref_wrap
-	{
-		T& ref;
-		explicit ref_wrap(const T& ref_): ref(ref_)
-			{}
-	};
-
-	namespace move_impl
+	namespace move_for_const_impl
 	{
 		template<class T>
-		rvalue_ref<T> move(const T& value)
+		rvalue_ref<const T> move(const T& value)
 		{
-			typedef rvalue_ref<T> rvalue_ref_const;
-			return rvalue_ref_const::move(value);
+			return rvalue_ref<const T>::move(value);
 		}
 	} // namespace move_impl
 
-	//using move_impl::move;
+	using move_for_const_impl::move;
 
 	template<class T>
 	rvalue_ref<T> move(T& value)
@@ -138,6 +126,7 @@ namespace stdex
 #define STDEX_DELETE_ICC() stdex_icc_deleter(true)
 
 #define STDEX_RV_REF(Type) stdex::rvalue_ref<Type>
+#define STDEX_RV_REF_CONST(Type) stdex::rvalue_ref<Type const>
 
 #ifdef _MSC_VER
 	#pragma warning (pop)
