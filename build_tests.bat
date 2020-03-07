6f742170749105b0ab7d7a1c6f0c509feeab6818
oktonion <0oktonion0@gmail.com>
2020-03-07 14:32:57 +0300

    failing builds for win-tests

--- build_tests.bat
+++ build_tests.bat
@@ -2,6 +2,7 @@ mkdir .\tests\bin
 mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
+@echo off
 
 set "build_ok=1"
 set INCLUDE=%INCLUDE%;%cd%\pthread\;
@@ -9,14 +10,18 @@ set INCLUDE=%INCLUDE%;%cd%\pthread\;
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test %VisualStudioVersion% %%~nf"
   cl -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
+  set origin_str=%%f
+  set replaced_str=!origin_str:fail=_!
   IF ERRORLEVEL 1 (
-    set "build_ok=0"
+    if not "%origin_str%"=="%replaced_str%" set "build_ok=0"
   )
 
   if "%build_ok%"=="1" (
     cl /I %cd%\pthread\ .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
+    set origin_str=%%f
+    set replaced_str=!origin_str:fail=_!
     IF ERRORLEVEL 1 (
-      set "build_ok=0"
+      if not "%origin_str%"=="%replaced_str%" set "build_ok=0"
     )
   )
 )
