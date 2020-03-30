742a01f3615e095f1ff2896a6ae0c853263ba3db
Alexander <0oktonion0@gmail.com>
2020-03-30 14:25:29 +0300

    Update move.hpp

--- stdex/include/move.hpp
+++ stdex/include/move.hpp
@@ -27,37 +27,37 @@ namespace stdex
 			deleted_implicit_copy_constructor_impl(const T &):
 				dummy(val) { (void)(dummy); }
 		};
-	} // namespace detail
 
-	typedef stdex::detail::deleted_implicit_copy_constructor_impl<char, 0> deleted_implicit_copy_constructor;
-	
-	class delete_implicit_copy_constructor:
-		deleted_implicit_copy_constructor
-	{
-	public:
-		delete_implicit_copy_constructor(const bool &):
-			deleted_implicit_copy_constructor(1)
-		{ }
-	};
+		typedef stdex::detail::deleted_implicit_copy_constructor_impl<char, 0> deleted_implicit_copy_constructor;
+		
+		class delete_implicit_copy_constructor:
+			deleted_implicit_copy_constructor
+		{
+		public:
+			delete_implicit_copy_constructor(const bool &):
+				deleted_implicit_copy_constructor(1)
+			{ }
+		};
+	} // namespace detail
 
 	namespace move_detail
 	{
 		template<class _Tp, bool Value>
-		struct rvalue_ref_base_impl
+		struct rvalue_reference_base_impl
 		{
 			typedef typename stdex::remove_const<_Tp>::type type;
 		};
 		template<class _Tp>
-		struct rvalue_ref_base_impl<_Tp, false>
+		struct rvalue_reference_base_impl<_Tp, false>
 		{
 			struct type { template<class T> type(T) {}};
 		};
 		template<class _Tp>
-		struct rvalue_ref_base
+		struct rvalue_reference_base
 		{
 			typedef 
 			typename 
-			rvalue_ref_base_impl<
+			rvalue_reference_base_impl<
 				_Tp, 
 				stdex::is_class<typename stdex::remove_const<_Tp>::type>::value
 			>::type type;
@@ -65,85 +65,91 @@ namespace stdex
 	} // namespace move_detail
 
     template<class _Tp>
-    class rvalue_ref:
-		public move_detail::rvalue_ref_base<_Tp>::type
+    class rvalue_reference:
+		public move_detail::rvalue_reference_base<_Tp>::type
     {
 	protected:
-		typedef typename move_detail::rvalue_ref_base<_Tp>::type base_type;
+		typedef typename move_detail::rvalue_reference_base<_Tp>::type base_type;
 		typedef typename stdex::remove_const<_Tp>::type value_type;
-		const value_type& _ref;
+		value_type& _ref;
 
-        explicit rvalue_ref(const value_type &ref) 
-			: base_type(ref),
-            _ref((ref)) 
+        explicit rvalue_reference(value_type &ref)
+            : base_type(ref)
+			, _ref((ref)) 
 		{ }
+		
     public:
-        rvalue_ref(const rvalue_ref<_Tp> &other)
-			: base_type(other._ref),
-			_ref(other._ref) 
-		{ }
-        //T& get() const {return _ref;}
+        rvalue_reference(const rvalue_reference<_Tp> &other)
+			: base_type(ref)
+			, _ref(other._ref) 
+		{ } 
+
 		operator value_type&() const {return _ref;}
-		//T* operator->() const {return &get();}
-		//T& operator*() const {return get();}
-		static rvalue_ref<value_type> move(value_type &value) 
+
+		static rvalue_reference<value_type> move(value_type &value) 
 		{
-			return rvalue_ref<value_type>(value);
+			return rvalue_reference<value_type>(value);
 		}
-		static rvalue_ref<const value_type> move(const value_type &value) 
+
+		static rvalue_reference<value_type> move(const rvalue_reference<value_type>& value)
 		{
-			return rvalue_ref<const value_type>(value);
-		}
-		static rvalue_ref<value_type> move(const rvalue_ref<value_type>& value)
-		{
-			return rvalue_ref<value_type>(value._ref);
+			return rvalue_reference<value_type>(value._ref);
 		}
     };
 
-	namespace move_for_const_impl
-	{
-		template<class _Tp>
-		rvalue_ref<const _Tp> move(const _Tp& value)
+    template<class _Tp>
+    class rvalue_reference <const _Tp>:
+		public move_detail::rvalue_reference_base<_Tp>::type
+    {
+	protected:
+		typedef typename move_detail::rvalue_reference_base<_Tp>::type base_type;
+		typedef const _Tp value_type;
+		value_type& _ref;
+
+        explicit rvalue_reference(value_type &ref)
+            : base_type(ref)
+			, _ref((ref)) 
+		{ }
+
+    public:
+        rvalue_reference(const rvalue_reference<_Tp> &other)
+			: base_type(ref)
+			, _ref(other._ref) 
+		{ }
+
+		operator value_type&() const {return _ref;}
+
+		static rvalue_reference<value_type> move(value_type &value) 
 		{
-			return rvalue_ref<const _Tp>::move(value);
+			return rvalue_reference<value_type>(value);
 		}
-	} // namespace move_for_const_impl
 
-	using move_for_const_impl::move;
+		static rvalue_reference<value_type> move(const rvalue_reference<value_type>& value)
+		{
+			return rvalue_reference<value_type>(value._ref);
+		}
+    };
 
 	template<class _Tp>
-	rvalue_ref<_Tp> move(_Tp& value)
+	rvalue_reference<_Tp> move(_Tp& value)
 	{
-		return rvalue_ref<_Tp>::move(value);
+		return rvalue_reference<_Tp>::move(value);
 	}
 
 	template<class _Tp>
-	rvalue_ref<_Tp> move(rvalue_ref<_Tp>& value)
+	rvalue_reference<_Tp> move(const rvalue_reference<_Tp>& value)
 	{
-		return rvalue_ref<_Tp>::move(value);
-	}
-
-	template<class _Tp>
-	rvalue_ref<_Tp> move(rvalue_ref<const _Tp>& value)
-	{
-		_Tp &ref = value;
-		return rvalue_ref<_Tp>::move(ref);
-	}
-
-	template<class _Tp>
-	rvalue_ref<_Tp> move(const rvalue_ref<_Tp>& value)
-	{
-		return rvalue_ref<_Tp>::move(value);
+		return rvalue_reference<_Tp>::move(value);
 	}
 }
 
 #define STDEX_NOT_COPYABLE \
-	stdex::delete_implicit_copy_constructor _stdex_icc_deleter;
+	stdex::detail::delete_implicit_copy_constructor _stdex_icc_deleter;
 
 #define STDEX_DELETE_ICC() _stdex_icc_deleter(true)
 
-#define STDEX_RV_REF(Type) stdex::rvalue_ref<Type>
-#define STDEX_RV_REF_CONST(Type) stdex::rvalue_ref<Type const>
+#define STDEX_RV_REF(Type) stdex::rvalue_reference<Type> 
+#define STDEX_RV_REF_CONST(Type) stdex::rvalue_reference<Type const> 
 
 #ifdef _MSC_VER
 	#pragma warning (pop)
