b29175650e4867b3da41761cc3ef616e9b4fd2f4
Alexander <0oktonion0@gmail.com>
2020-03-30 14:57:59 +0300

    Update move.hpp

--- stdex/include/move.hpp
+++ stdex/include/move.hpp
@@ -26,7 +26,7 @@ namespace stdex
 		public:
 			deleted_implicit_copy_constructor_impl(const T &):
 				dummy(val) { (void)(dummy); }
-			deleted_implicit_copy_constructor_impl &operator=(deleted_implicit_copy_constructor_impl &other)
+			deleted_implicit_copy_constructor_impl &operator=(const deleted_implicit_copy_constructor_impl &)
 			{ return *this; }
 		};
 
