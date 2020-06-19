79148bf9349490c4d7f6e1d8f9f55f314bdab365
Alexander <0oktonion0@gmail.com>
2020-06-19 23:46:59 +0300

    correct build scripts for windows

--- build_tests.bat
+++ build_tests.bat
@@ -2,60 +2,79 @@ mkdir .\tests\bin
 mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
-rem @ECHO OFF
+@ECHO OFF
 
-set "build_ok=1"
+set true=1==1
+set false=1==0
+
+set build_ok=!true!
+
 set INCLUDE=%INCLUDE%;%cd%\pthread\;
 
 set "tests_failed=failed tests:"
+set has_compile_error=!false!
+set has_compile_warn=!false!
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test %VisualStudioVersion% %%~nf"
-  set "has_compile_error=0"
-  set "has_compile_warn=0"
-  cl -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
-  @if ERRORLEVEL != 0 (
-    set "has_compile_error=1"
+  set has_compile_error=!false!
+  set has_compile_warn=!false!
+  cl -nologo -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
+  if not !errorlevel!==0 (
+    set has_compile_error=!true!
   )
-  call :CheckOutput %%~nf
 
-  if %build_ok%==1 (
-    set "has_compile_error=0"
-    set "has_compile_warn=0"
-    cl /I %cd%\pthread\ .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
-    @if ERRORLEVEL != 0 (
-      set "has_compile_error=1"
+  if !has_compile_error!==!true! (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=!"
+    if "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      echo "failed as expected"
     )
-    call :CheckOutput %%~nf
+  ) else (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=_!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      echo "not failed as expected"
+      set "tests_failed=!tests_failed! !origin_str!"
+    )
+  )
+  
+  if !has_compile_error!==!false! (
+    if !build_ok!==!true! (
+      cl -nologo /I %cd%\pthread\ .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
+      if not !errorlevel!==0 (
+        set has_compile_error=!true!
+      )
+
+      if !has_compile_error!==!true! (
+        set "origin_str=%%~nf"
+        set "replaced_str=!origin_str:fail=!"
+        if "!origin_str!"=="!replaced_str!" (
+          set build_ok=!false!
+          set "tests_failed=!tests_failed! !origin_str!"
+        ) else (
+          echo "failed as expected"
+        )
+      ) else (
+        set "origin_str=%%~nf"
+        set "replaced_str=!origin_str:fail=_!"
+        if not "!origin_str!"=="!replaced_str!" (
+          set build_ok=!false!
+          echo "not failed as expected"
+          set "tests_failed=!tests_failed! !origin_str!"
+        )
+      )
+    )
   )
 )
 
-if %build_ok% NEQ 1 (
-  echo "%tests_failed%"
+if !build_ok!==!false! (
+  echo !tests_failed!
   exit /B 1
 )
 
 del /Q .\tests\obj\*.obj
-
-:CheckOutput    - here starts the function
-
-    if %has_compile_error%==1 (
-      set "origin_str=%1"
-      set "replaced_str=!origin_str:fail=!"
-      if "!origin_str!" == "!replaced_str!" (
-        set "build_ok=0"
-        set "tests_failed=!tests_failed! %1"
-      ) else (
-        echo "failed as expected"
-      )
-    ) else (
-      set "origin_str=%1"
-      set "replaced_str=!origin_str:fail=_!"
-      if not "!origin_str!" == "!replaced_str!" (
-        set "build_ok=0"
-        echo "not failed as expected"
-        echo !MYOUTPUT!
-        set "tests_failed=!tests_failed! %1"
-      )
-    )
-goto:eof
--- build_tests_bcc32.bat
+++ build_tests_bcc32.bat
@@ -5,7 +5,10 @@ setlocal enabledelayedexpansion
 
 @ECHO OFF
 
-set "build_ok=1"
+set true=1==1
+set false=1==0
+
+set build_ok=!true!
 
 set LF=^
 
@@ -13,76 +16,99 @@ set LF=^
 REM The two empty lines are required here
 set "MYOUTPUT="
 set "tests_failed=failed tests:"
+set has_compile_error=!false!
+set has_compile_warn=!false!
+
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test Borland C++ %%~nf"
   set "MYOUTPUT="
-  set "has_compile_error=0"
-  set "has_compile_warn=0"
+  set has_compile_error=!false!
+  set has_compile_warn=!false!
   for /f "delims=" %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -n.\tests\obj\ -L.\stdex\lib\ -I%cd%\pthread\ -c ".\tests\%%f"') do (
     if defined MYOUTPUT set "MYOUTPUT=!MYOUTPUT!!LF!"
     set "MYOUTPUT=!MYOUTPUT!%%i"
     set "origin_str=!MYOUTPUT!"
     set "replaced_str=!origin_str:Error E=!"
-    if not "!origin_str!" == "!replaced_str!" (
-      set "has_compile_error=1"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
     )
     set "replaced_str=!origin_str:Warning W=!"
     if not "!origin_str!"=="!replaced_str!" (
-      set "has_compile_warn=1"
+      set has_compile_warn=!true!
     )
   )
-  call :CheckOutput %%~nf
-
-  if %build_ok%==1 (
-    set "MYOUTPUT="
-    set "has_compile_error=0"
-    for /f %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -L.\stdex\lib\ -lap -I%cd%\pthread\ -e.\tests\bin\%%~nf.exe stdex.lib cw32mt.lib ntdll.lib .\tests\obj\%%~nf.obj') do (
-      if defined MYOUTPUT set "MYOUTPUT=!MYOUTPUT!!LF!"
-      set "MYOUTPUT=!MYOUTPUT!%%i"
-      set "origin_str=!MYOUTPUT!"
-      set "replaced_str=!origin_str:Error E=!"
-      if not "!origin_str!" == "!replaced_str!" (
-        set "has_compile_error=1"
-      )
-      set "replaced_str=!origin_str:Warning W=!"
-      if not "!origin_str!"=="!replaced_str!" (
-        set "has_compile_warn=1"
-      )
+  
+  if !has_compile_error!==!true! (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=!"
+    if "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      echo !MYOUTPUT!
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      echo "failed as expected"
     )
-    call :CheckOutput %%~nf
+  ) else (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=_!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      echo "not failed as expected"
+      echo !MYOUTPUT!
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      if !has_compile_warn!==!true! echo !MYOUTPUT!
+    )
+  )
+  
+  if !has_compile_error!==!false! (
+    if !build_ok!==!true! (
+      set "MYOUTPUT="
+      for /f %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -L.\stdex\lib\ -lap -I%cd%\pthread\ -e.\tests\bin\%%~nf.exe stdex.lib cw32mt.lib ntdll.lib .\tests\obj\%%~nf.obj') do (
+        if defined MYOUTPUT set "MYOUTPUT=!MYOUTPUT!!LF!"
+        set "MYOUTPUT=!MYOUTPUT!%%i"
+        set "origin_str=!MYOUTPUT!"
+        set "replaced_str=!origin_str:Error E=!"
+        if not "!origin_str!"=="!replaced_str!" (
+          set has_compile_error=!true!
+        )
+        set "replaced_str=!origin_str:Warning W=!"
+        if not "!origin_str!"=="!replaced_str!" (
+          set has_compile_warn=!true!
+        )
+      )
+      
+      if !has_compile_error!==!true! (
+        set "origin_str=%%~nf"
+        set "replaced_str=!origin_str:fail=!"
+        if "!origin_str!"=="!replaced_str!" (
+          set build_ok=!false!
+          echo !MYOUTPUT!
+          set "tests_failed=!tests_failed! !origin_str!"
+        ) else (
+          echo "failed as expected"
+        )
+      ) else (
+        set "origin_str=%%~nf"
+        set "replaced_str=!origin_str:fail=_!"
+        if not "!origin_str!"=="!replaced_str!" (
+          set build_ok=!false!
+          echo "not failed as expected"
+          echo !MYOUTPUT!
+          set "tests_failed=!tests_failed! !origin_str!"
+        ) else (
+          if !has_compile_warn!==!true! echo !MYOUTPUT!
+        )
+      )
+
+    )
   )
 )
 
-if %build_ok% NEQ 1 (
-  echo "%tests_failed%"
+if !build_ok!==!false! (
+  echo !tests_failed!
   exit /B 1
 )
 
 del /Q .\tests\obj\*.obj
-
-:CheckOutput    - here starts the function
-
-    if %has_compile_error%==1 (
-      set "origin_str=%1"
-      set "replaced_str=!origin_str:fail=!"
-      if "!origin_str!" == "!replaced_str!" (
-        set "build_ok=0"
-        echo !MYOUTPUT!
-        set "tests_failed=!tests_failed! %1"
-      ) else (
-        echo "failed as expected"
-      )
-    ) else (
-      set "origin_str=%1"
-      set "replaced_str=!origin_str:fail=_!"
-      if not "!origin_str!" == "!replaced_str!" (
-        set "build_ok=0"
-        echo "not failed as expected"
-        echo !MYOUTPUT!
-        set "tests_failed=!tests_failed! %1"
-      ) else (
-        if %has_compile_warn%==1 echo !MYOUTPUT!
-      )
-    )
-goto:eof
