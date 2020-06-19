b6e42462686e87fce404348cf1de0fe176cc91c4
Alexander <0oktonion0@gmail.com>
2020-06-19 19:30:47 +0300

    Update build_tests.bat

--- build_tests.bat
+++ build_tests.bat
@@ -7,29 +7,24 @@ setlocal enabledelayedexpansion
 set "build_ok=1"
 set INCLUDE=%INCLUDE%;%cd%\pthread\;
 
-set "MYOUTPUT="
 set "tests_failed=failed tests:"
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test %VisualStudioVersion% %%~nf"
-  set "MYOUTPUT="
   set "has_compile_error=0"
   set "has_compile_warn=0"
   cl -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
   @if ERRORLEVEL != 0 (
     set "has_compile_error=1"
-    set "MYOUTPUT=compilation failed"
   )
   call :CheckOutput %%~nf
 
   if %build_ok%==1 (
-    set "MYOUTPUT="
     set "has_compile_error=0"
     set "has_compile_warn=0"
     cl /I %cd%\pthread\ .\tests\obj\%%~nf.obj stdex.lib ntdll.lib -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe -link -LIBPATH:.\stdex\lib
     @if ERRORLEVEL != 0 (
       set "has_compile_error=1"
-      set "MYOUTPUT=compilation failed"
     )
     call :CheckOutput %%~nf
   )
@@ -49,7 +44,6 @@ del /Q .\tests\obj\*.obj
       set "replaced_str=!origin_str:fail=!"
       if "!origin_str!" == "!replaced_str!" (
         set "build_ok=0"
-        echo !MYOUTPUT!
         set "tests_failed=!tests_failed! %1"
       ) else (
         echo "failed as expected"
@@ -62,8 +56,6 @@ del /Q .\tests\obj\*.obj
         echo "not failed as expected"
         echo !MYOUTPUT!
         set "tests_failed=!tests_failed! %1"
-      ) else (
-        if %has_compile_warn%==1 echo !MYOUTPUT!
       )
     )
 goto:eof
