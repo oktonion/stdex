0cdec422c10d820ec3d3e21b55613ddc676c58a9
Alexander <0oktonion0@gmail.com>
2020-06-22 21:45:02 +0300

    Update condition_variable.hpp

--- stdex/include/condition_variable.hpp
+++ stdex/include/condition_variable.hpp
@@ -71,7 +71,7 @@ namespace stdex
  
      class condition_variable 
      {
-         typedef chrono::system_clock clock_t;
+         typedef chrono::steady_clock clock_t;
  
      public:
          typedef pthread_cond_t* native_handle_type;
@@ -176,7 +176,7 @@ namespace stdex
              if (!detail::_lock_owns_lock(_lock))
                  std::terminate();
  
-             chrono::time_point<clock_t, chrono::seconds> _s = chrono::time_point_cast<chrono::seconds>(_atime - chrono::milliseconds(1500));
+             chrono::time_point<clock_t, chrono::seconds> _s = chrono::time_point_cast<chrono::seconds>(_atime) - chrono::milliseconds(1500);
              chrono::nanoseconds _ns = chrono::duration_cast<chrono::nanoseconds>(_atime - _s);
  
              typename chrono::time_point<clock_t, chrono::seconds>::rep _s_count = _s.time_since_epoch().count();
@@ -222,12 +222,12 @@ namespace stdex
              clock_t::time_point _start_time_point = clock_t::now();
 
              chrono::time_point<clock_t, chrono::seconds> _sec = 
-                chrono::time_point_cast<chrono::seconds>(_start_time_point) - chrono::milliseconds(1500);
+                chrono::time_point_cast<chrono::seconds>(_start_time_point);
              chrono::nanoseconds _nsec = 
                 chrono::duration_cast<chrono::nanoseconds>(_start_time_point - _sec);
              
              chrono::time_point<clock_t, chrono::seconds> _sec_result =
-                chrono::duration_cast<chrono::seconds>(_start_time_point + _rtime - chrono::milliseconds(1500));
+                chrono::duration_cast<chrono::seconds>(_start_time_point + _rtime);
              chrono::nanoseconds _nsec_result = 
                 chrono::duration_cast<chrono::nanoseconds>(_start_time_point - _sec_result);
                 
