e97d672f8a188073440f47f4bb98800253f4f42a
Alexander <0oktonion0@gmail.com>
2020-06-22 22:13:00 +0300

    Update condition_variable.hpp

--- stdex/include/condition_variable.hpp
+++ stdex/include/condition_variable.hpp
@@ -169,7 +169,7 @@ namespace stdex
              if (!detail::_lock_owns_lock(_lock))
                  std::terminate();
  
-             chrono::time_point<clock_t, chrono::seconds> _s = chrono::time_point_cast<chrono::seconds>(_atime) - chrono::milliseconds(1500);
+             chrono::time_point<clock_t, chrono::seconds> _s = chrono::time_point_cast<chrono::seconds>(_atime);
              chrono::nanoseconds _ns = chrono::duration_cast<chrono::nanoseconds>(_atime - _s);
  
              typename chrono::time_point<clock_t, chrono::seconds>::rep _s_count = _s.time_since_epoch().count();
