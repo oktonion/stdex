d80580b73318c7a660ff03ca327d82b4232be4b6
oktonion <0oktonion0@gmail.com>
2020-07-03 10:36:01 +0300

    Update build_tests_bcc32.bat

--- build_tests_bcc32.bat
+++ build_tests_bcc32.bat
@@ -15,6 +15,8 @@ set LF=^
 
 REM The two empty lines are required here
 set "MYOUTPUT="
+set "MYOUTPUT2="
+set "MYOUTPUT3="
 set "tests_failed=unsuccessful tests:"
 set has_compile_error=!false!
 set has_compile_warn=!false!
@@ -23,8 +25,11 @@ set has_compile_warn=!false!
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test Borland C++ %%~nf"
   set "MYOUTPUT="
+  set "MYOUTPUT2="
+  set "MYOUTPUT3="
   set has_compile_error=!false!
   set has_compile_warn=!false!
+  set /A line_n=0
   for /f "delims=" %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -n.\tests\obj\ -L.\stdex\lib\ -I%cd%\pthread\ -c ".\tests\%%f"') do (
     if defined MYOUTPUT set "MYOUTPUT=!MYOUTPUT!!LF!"
     set "MYOUTPUT=!MYOUTPUT!%%i"
@@ -37,6 +42,20 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
     if not "!origin_str!"=="!replaced_str!" (
       set has_compile_warn=!true!
     )
+    set /A line_n=!line_n!+1
+    if !line_n! GEQ 50 (
+      if !line_n! GEQ 100 (
+        if "!MYOUTPUT2!"=="" (
+          set "MYOUTPUT2=!MYOUTPUT!"
+          set "MYOUTPUT="
+        )
+      ) else (
+        if "!MYOUTPUT3!"=="" (
+          set "MYOUTPUT3=!MYOUTPUT!"
+          set "MYOUTPUT="
+        )
+      )
+    )
   )
   
   if !has_compile_error!==!true! (
@@ -44,6 +63,8 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
     set "replaced_str=!origin_str:fail=!"
     if "!origin_str!"=="!replaced_str!" (
       set build_ok=!false!
+      echo !MYOUTPUT3!
+      echo !MYOUTPUT2!
       echo !MYOUTPUT!
       set "tests_failed=!tests_failed! !origin_str!"
     ) else (
@@ -55,6 +76,8 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
     if not "!origin_str!"=="!replaced_str!" (
       set build_ok=!false!
       echo "not failed as expected"
+      echo !MYOUTPUT3!
+      echo !MYOUTPUT2!
       echo !MYOUTPUT!
       set "tests_failed=!tests_failed! !origin_str!"
     ) else (
@@ -64,6 +87,9 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
   
   if !has_compile_error!==!false! (
     set "MYOUTPUT="
+    set "MYOUTPUT2="
+    set "MYOUTPUT3="
+    set /A line_n=0
     for /f %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -L.\stdex\lib\ -lap -I%cd%\pthread\ -e.\tests\bin\%%~nf.exe stdex.lib cw32mt.lib ntdll.lib .\tests\obj\%%~nf.obj') do (
       if defined MYOUTPUT set "MYOUTPUT=!MYOUTPUT!!LF!"
       set "MYOUTPUT=!MYOUTPUT!%%i"
@@ -76,6 +102,16 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
       if not "!origin_str!"=="!replaced_str!" (
         set has_compile_warn=!true!
       )
+      set /A line_n=!line_n!+1
+      if !line_n! GEQ 50 (
+        if !line_n! GEQ 100 (
+          set "MYOUTPUT2=!MYOUTPUT!"
+          set "MYOUTPUT="
+        ) else (
+          set "MYOUTPUT3=!MYOUTPUT!"
+          set "MYOUTPUT="
+        )
+      )
     )
     
     if !has_compile_error!==!true! (
@@ -83,6 +119,8 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
       set "replaced_str=!origin_str:fail=!"
       if "!origin_str!"=="!replaced_str!" (
         set build_ok=!false!
+        echo !MYOUTPUT3!
+        echo !MYOUTPUT2!
         echo !MYOUTPUT!
         set "tests_failed=!tests_failed! !origin_str!"
       ) else (
@@ -94,10 +132,16 @@ for /f %%f in ('dir /b ".\tests\*.cpp"') do (
       if not "!origin_str!"=="!replaced_str!" (
         set build_ok=!false!
         echo "not failed as expected"
+        echo !MYOUTPUT3!
+        echo !MYOUTPUT2!
         echo !MYOUTPUT!
         set "tests_failed=!tests_failed! !origin_str!"
       ) else (
-        if !has_compile_warn!==!true! echo !MYOUTPUT!
+        if !has_compile_warn!==!true! (
+          echo !MYOUTPUT3!
+          echo !MYOUTPUT2!
+          echo !MYOUTPUT!
+        )
       )
     )
   )
