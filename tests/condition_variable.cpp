26d89b09fcf7a8a637cbd6cf373a0fc9cbbf824f
oktonion <0oktonion0@gmail.com>
2020-07-16 14:10:38 +0300

    Update condition_variable.cpp

--- tests/condition_variable.cpp
+++ tests/condition_variable.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #define DYNAMIC_VERIFY(cond) if(!(cond)) {std::cout << "check condition \'" << #cond << "\' failed at line " << __LINE__ << std::endl; return __LINE__;}
 #define RUN_TEST(test) {std::cout << #test << std::endl; int line = test(); if(line != 0) {return line;}}
+#define DYNAMIC_VERIFY_FAIL {std::cout << "check condition " << "failed at line " << __LINE__ << std::endl; return -1;}
 
 namespace cond_var_tests
 {
