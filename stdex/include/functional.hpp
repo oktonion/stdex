a6295ded4934681a45932c79d5e2252018439d92
Alexander <oktonion.work@gmail.com>
2020-10-28 16:40:22 +0300

    Update functional.hpp

--- stdex/include/functional.hpp
+++ stdex/include/functional.hpp
@@ -1,36 +1,36 @@
 #ifndef _STDEX_FUNCTIONAL_H
 #define _STDEX_FUNCTIONAL_H
  
- #if _MSC_VER > 1000
- #pragma once
- #endif // _MSC_VER > 1000
- 
- // stdex includes
- #include "./core.h"
- #include "./cstdint.hpp"
- #include "./type_traits.hpp"
- 
- // POSIX includes
- 
- // std includes
- #include <functional>
- #include <bitset> // for hash
- #include <vector> // for hash
- 
- #ifdef _STDEX_NATIVE_CPP11_SUPPORT
- 
- #define _STDEX_DELETED_FUNCTION =delete
- #define _STDEX_NOEXCEPT_FUNCTION noexcept
- 
- #else
- 
- #define _STDEX_DELETED_FUNCTION 
- #define _STDEX_NOEXCEPT_FUNCTION throw()
- 
- #endif
- 
- namespace stdex
- {
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// stdex includes
+#include "./core.h"
+#include "./cstdint.hpp"
+#include "./type_traits.hpp"
+
+// POSIX includes
+
+// std includes
+#include <functional>
+#include <bitset> // for hash
+#include <vector> // for hash
+
+#ifdef _STDEX_NATIVE_CPP11_SUPPORT
+
+#define _STDEX_DELETED_FUNCTION =delete
+#define _STDEX_NOEXCEPT_FUNCTION noexcept
+
+#else
+
+#define _STDEX_DELETED_FUNCTION 
+#define _STDEX_NOEXCEPT_FUNCTION throw()
+
+#endif
+
+namespace stdex
+{
     namespace functional_cpp11
     {
     #ifndef STDEX_DO_NOT_ADD_CPP11_STD // define to exclude std implementations
@@ -575,7 +575,7 @@
         };
 
     } // namespace detail
-    
+
 
     using detail::function;
 
@@ -830,7 +830,7 @@
     //    }
     //};
 
-    
+
     // Arithmetic operations
 
     using std::plus;
@@ -907,11 +907,10 @@
             return (~_left);
         }
     };
-    
- } // namespace stdex
- 
- #undef _STDEX_DELETED_FUNCTION
- #undef _STDEX_NOEXCEPT_FUNCTION
- 
- #endif // _STDEX_FUNCTIONAL_H
- 
+
+} // namespace stdex
+
+#undef _STDEX_DELETED_FUNCTION
+#undef _STDEX_NOEXCEPT_FUNCTION
+
+#endif // _STDEX_FUNCTIONAL_H
