af6c036331b343c83f4d714d528a4977fd765a72
Aleksandr <oktonion.work@gmail.com>
2020-12-01 09:33:32 -0500

    Update is_const.cpp
    tests for function pointers

--- tests/is_const.cpp
+++ tests/is_const.cpp
@@ -3,8 +3,13 @@
 
 struct ClassType {};
 typedef const ClassType cClassType;
+typedef const ClassType& cClassTypeRef;
 typedef const volatile ClassType cvClassType;
 typedef volatile ClassType vClassType;
+typedef void(*FuncPtrType)(int);
+typedef typename stdex::remove_pointer<FuncPtrType>::type FuncType;
+typedef const FuncPtrType cFuncPtrType;
+typedef const FuncType cFuncType;
 
 int main(void)
 {
@@ -16,11 +21,23 @@ int main(void)
     STATIC_ASSERT(is_const<const volatile int>::value == (true), should_be_const);
     STATIC_ASSERT(is_const<cClassType>::value == (true), should_be_const);
     STATIC_ASSERT(is_const<cvClassType>::value == (true), should_be_const);
+
+    STATIC_ASSERT(is_const<cFuncPtrType>::value == (true), should_be_const);
                             
     // Negative tests.      
     STATIC_ASSERT(is_const<int>::value == (false), can_not_be_const);
     STATIC_ASSERT(is_const<volatile int>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<const volatile int&>::value == (false), can_not_be_const);
     STATIC_ASSERT(is_const<ClassType>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<cClassTypeRef>::value == (false), can_not_be_const);
     STATIC_ASSERT(is_const<vClassType>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<vClassType&>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<const vClassType&>::value == (false), can_not_be_const);
+
+    STATIC_ASSERT(is_const<FuncPtrType>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<FuncType>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<cFuncType>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<cFuncPtrType&>::value == (false), can_not_be_const);
+    STATIC_ASSERT(is_const<cFuncType&>::value == (false), can_not_be_const);
     return 0;
 }
