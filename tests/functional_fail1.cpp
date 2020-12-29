eb91b98a70561a3c32364a1d6e53c8ea0c7ca007
oktonion <0oktonion0@gmail.com>
2020-12-29 12:54:40 +0300

    Create functional_fail1.cpp

---
+++ tests/functional_fail1.cpp
@@ -0,0 +1,15 @@
+#include "../stdex/include/core.h"
+#include "../stdex/include/functional.hpp"
+
+int main()
+{
+    struct lambdas
+    {
+        static void func_none(){}
+        static void func0(int, float){}
+    };
+
+    std::bind(&lambdas::func_none, 0)();
+
+    return 0;
+}
