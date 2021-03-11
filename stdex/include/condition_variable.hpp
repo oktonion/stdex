73da96129cd103764fae64f232e9563becd0f3be
oktonion <0oktonion0@gmail.com>
2021-03-11 10:19:04 +0300

    Update condition_variable.hpp

--- stdex/include/condition_variable.hpp
+++ stdex/include/condition_variable.hpp
@@ -254,7 +254,7 @@ namespace stdex
             _ts.tv_sec = _tp_as_ts.tv_sec;
             _ts.tv_nsec = _tp_as_ts.tv_nsec;
 
-            if ((time_measure_clock_t::now() - _start_time_point) >= _rtime)
+            if ((time_measure_clock_t::now() - _start_time_point) > _rtime)
                 return cv_status::timeout;
 
             int _err = 
