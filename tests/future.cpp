9d55c532727ce185f94661675ef4a39882a35832
oktonion <0oktonion0@gmail.com>
2021-04-25 23:47:36 +0300

    Update future.cpp

--- tests/future.cpp
+++ tests/future.cpp
@@ -11,19 +11,6 @@
 #define DYNAMIC_VERIFY_FAIL {std::cout << "check condition " << "failed at line " << __LINE__ << std::endl; return -1;}
 #define DYNAMIC_VERIFY_ABORT(cond) if(!(cond)) {std::cout << "check condition \'" << #cond << "\' failed at line " << __LINE__ << std::endl; std::abort();}
 
-#include "../stdex/include/core.h"
-#include "../stdex/include/future.hpp"
-#include "../stdex/include/chrono.hpp"
-
-#include <iostream>
-#include <string>
-
-#define VERIFY(cond) STATIC_ASSERT((cond), check)
-#define DYNAMIC_VERIFY(cond) if(!(cond)) {std::cout << "check condition \'" << #cond << "\' failed at line " << __LINE__ << std::endl; return -1;}
-#define RUN_TEST(test) {std::cout << #test << std::endl; int line = test(); if(line != 0) {std::cout << "failed at line " << line << std::endl; return line;}}
-#define DYNAMIC_VERIFY_FAIL {std::cout << "check condition " << "failed at line " << __LINE__ << std::endl; return -1;}
-#define DYNAMIC_VERIFY_ABORT(cond) if(!(cond)) {std::cout << "check condition \'" << #cond << "\' failed at line " << __LINE__ << std::endl; std::abort();}
-
 int test01()
 {
   stdex::promise<int> p;
