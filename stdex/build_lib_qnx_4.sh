12e9dbf36071e3ea360173bb7b4e9e08ef1f7a0a
oktonion <0oktonion0@gmail.com>
2021-06-03 08:58:11 +0300

    Create build_lib_qnx_4.sh

---
+++ stdex/build_lib_qnx_4.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+COMPILER=g++
+
+$COMPILER --version
+
+mkdir ./obj
+
+
+build_ok=1
+
+
+for file in ./src/*.cpp; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "compiling $filename"
+  if $COMPILER -pedantic $exclude_warn -lstdlib -c "$file" -o "./obj/$filename.o"; then
+    echo ""
+  else
+    build_ok=0
+  fi
+done
+
+if [ $build_ok -eq 0 ]; then
+  echo "libstdex.a build failed"
+  exit 1
+fi
+
+mkdir ./lib
+
+libname="rcs ./lib/libstdex.a"
+
+for file in ./obj/*.o; do
+  filename=$(basename -- "$file")
+  filename="${filename%.*}"
+  echo "archiving $filename to libstdex.a"
+  wlib $libname +$filename
+done
+
+ls ./obj
+
+ls ./lib
+
+echo "lib done"
+echo "contains:"
+ar -t "./lib/libstdex.a"
+
+
