7a58a16bd3166c95914dcf9fbd9f311f8630e777
oktonion <0oktonion0@gmail.com>
2021-07-20 10:51:18 +0300

    Update get_code_cov.sh

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for filename in `find . | egrep '\.h*'`; 
-do 
-  gcov -n -o . $filename > /dev/null; 
+#for filename in `find ./tests/ | egrep '\.cpp'`; 
+#do 
+ # gcov -n -o ./tests/bin/ $filename > /dev/null; 
+#done
+
+for file in ./tests/bin/*; do
+    gcov $file.gcda
 done
