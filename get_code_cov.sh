ee9383879c3ab9564a1d78588dd4bdf17be4d381
oktonion <0oktonion0@gmail.com>
2021-09-17 12:31:52 +0300

    Update get_code_cov.sh

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -4,10 +4,16 @@
  # gcov -n -o ./tests/bin/ $filename > /dev/null; 
 #done
 
-#cp *.gcda ./tests/bin/
-#cp *.gcno ./tests/bin/
 
 
-for file in ./tests/bin/*; do
+mkdir ./tests/tmp
+
+cp ./tests/bin/* ./tests/tmp/
+cp *.gcda ./tests/bin/
+cp *.gcno ./tests/bin/
+
+cd ./tests/bin/
+
+for file in ./../../tests/tmp/*; do
     gcov -abcfu ./$file.gcda
 done
