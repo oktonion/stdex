848f635e965dde33331fda1fb5ae30dd040179fe
oktonion <0oktonion0@gmail.com>
2023-01-13 10:53:06 +0300

    Update future_poll.cpp

--- tests/future_poll.cpp
+++ tests/future_poll.cpp
@@ -166,7 +166,9 @@ int main()
   for(int i = 0; i < iterations; i++)
     f.wait_for(chrono::seconds(0));
   chrono::high_resolution_clock::time_point stop = chrono::high_resolution_clock::now();
-
+  std::cout << "We've run too few iterations for the clock resolution. \n"
+     "Attempt to calibrate it." << std::endl;
+  print("wait_for(0s) next iteration", stop - start);
   /* We've run too few iterations for the clock resolution.
      Attempt to calibrate it.  */
   if (start == stop)
