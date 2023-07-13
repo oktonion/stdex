77d4bdea8d0be7b1e31b376a7371f3607ad59d98
oktonion <0oktonion0@gmail.com>
2023-07-13 10:37:19 +0300

    Update type_traits.hpp

--- stdex/include/type_traits.hpp
+++ stdex/include/type_traits.hpp
@@ -3255,7 +3255,7 @@ namespace stdex
         template<class _Tp>
         _Tp _declval();
 
-        template<class _Tp, class _U, int Size = sizeof(false ? _declval<_U>() : _declval<_Tp>()) >
+        template<class _Tp, class _U, int Size = sizeof(false ? _U() : _Tp()) >
         struct _common_other_type_impl1_any_value {
             _common_other_type_impl1_any_value(_Tp) {}
         };
