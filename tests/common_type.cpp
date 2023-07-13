9f225182efbeb2bda80e3e98549aee460c28b046
oktonion <0oktonion0@gmail.com>
2023-07-13 09:23:58 +0300

    Update common_type.cpp

--- tests/common_type.cpp
+++ tests/common_type.cpp
@@ -9,10 +9,106 @@
 
 struct A { };
 struct B : A { };
+struct C : A { };
+struct D : B { };
+
+template<class type1>
+void COMMON_TYPE_TEST_1_CPP98() 
+{
+  using namespace stdex;
+#define uid cpp98
+  typedef common_type<type1>::type JOIN(test_t,uid); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,uid), JOIN(test_t,uid)>::value), check_1);} 
+  typedef common_type<const type1>::type JOIN(test_t,JOIN(uid,c)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,c)), 
+                   JOIN(test_t,JOIN(uid,c))>::value), check_2);} 
+  typedef common_type<volatile type1>::type JOIN(test_t,JOIN(uid,v)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,v)), 
+                   JOIN(test_t,JOIN(uid,v))>::value), check_3);} 
+  typedef common_type<const volatile type1>::type JOIN(test_t,JOIN(uid,cv)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,cv)), 
+                   JOIN(test_t,JOIN(uid,cv))>::value), check_4);} 
+  typedef common_type<type1 &>::type JOIN(test_t,JOIN(uid,l)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,l)), 
+                   JOIN(test_t,JOIN(uid,l))>::value), check_5);} 
+  typedef common_type<const type1 &>::type JOIN(test_t,JOIN(uid,lc)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,lc)), 
+                   JOIN(test_t,JOIN(uid,lc))>::value), check_6);} 
+  typedef common_type<volatile type1 &>::type JOIN(test_t,JOIN(uid,lv)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,lv)), 
+                   JOIN(test_t,JOIN(uid,lv))>::value), check_7);} 
+  typedef common_type<const volatile type1 &>::type JOIN(test_t,JOIN(uid,lcv)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,lcv)), 
+                   JOIN(test_t,JOIN(uid,lcv))>::value), check_8);} 
+#undef uid
+}
+
+template<class type1>
+void COMMON_TYPE_TEST_1_CPP11() 
+{
+  using namespace stdex;
+#define uid cpp11
+  typedef common_type<type1 &&>::type JOIN(test_t,JOIN(uid,r)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,r)), 
+                   JOIN(test_t,JOIN(uid,r))>::value), check_9);} 
+  typedef common_type<const type1 &&>::type JOIN(test_t,JOIN(uid,rc)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,rc)), 
+                   JOIN(test_t,JOIN(uid,rc))>::value), check_10);} 
+  typedef common_type<volatile type1 &&>::type JOIN(test_t,JOIN(uid,rv)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,rv)), 
+                   JOIN(test_t,JOIN(uid,rv))>::value), check_11);} 
+  typedef common_type<const volatile type1 &&>::type JOIN(test_t,JOIN(uid,rcv)); 
+  {STATIC_ASSERT( (is_same<JOIN(test_t,JOIN(uid,rcv)), 
+                   JOIN(test_t,JOIN(uid,rcv))>::value), check_12);}
+#undef uid
+}
+
+template<class type1, class type2, class type3>
+void COMMON_TYPE_TEST_2_IMPL()
+{
+  using namespace stdex;
+#define uid COMMON_TYPE_TEST_2_IMPL
+  typedef typename common_type<type1, type2>::type  	JOIN(JOIN(test, uid),_t1);
+  typedef typename common_type<type2, type1>::type  	JOIN(JOIN(test, uid),_t2);
+  {STATIC_ASSERT( (is_same<JOIN(JOIN(test, uid),_t1), type3>::value), check1);}
+  {STATIC_ASSERT( (is_same<JOIN(JOIN(test, uid),_t2), type3>::value), check2);}
+#undef uid
+}
+
+
+#ifdef _STDEX_NATIVE_CPP11_SUPPORT
+
+#define COMMON_TYPE_TEST_1(type1) \
+    COMMON_TYPE_TEST_1_CPP98<type1>(); \
+    COMMON_TYPE_TEST_1_CPP11<type1>()
+
+#define COMMON_TYPE_TEST_2(cv_qual, type1, type2, type3) \
+  COMMON_TYPE_TEST_2_IMPL<cv_qual type1, type2, type3>(); \
+  COMMON_TYPE_TEST_2_IMPL<cv_qual type1 &, type2, type3>(); \
+  COMMON_TYPE_TEST_2_IMPL<cv_qual type1 &&, type2, type3>()
+
+#else
+
+#define COMMON_TYPE_TEST_1(type1) \
+    COMMON_TYPE_TEST_1_CPP98<type1>()
+
+#define COMMON_TYPE_TEST_2(cv_qual, type1, type2, type3) \
+  COMMON_TYPE_TEST_2_IMPL<cv_qual type1, type2, type3>(); \
+  COMMON_TYPE_TEST_2_IMPL<cv_qual type1 &, type2, type3>();
+
+#endif
 
 int main(void)
 {
     using namespace stdex;
+
+    // Positive tests.
+    {
+        COMMON_TYPE_TEST_1(int);
+        COMMON_TYPE_TEST_1(double);
+        COMMON_TYPE_TEST_1(A);
+        COMMON_TYPE_TEST_1(B);
+    }
     
     // common_type
    {
@@ -198,7 +294,6 @@ int main(void)
         VERIFY((is_same<test_c_uid_t1_ref, type3>::value));
         VERIFY((is_same<test_c_uid_t2_ref, type3>::value));
 
-        /*
         VERIFY((is_same<test_v_uid_t1, type3>::value));
         VERIFY((is_same<test_v_uid_t2, type3>::value));
         VERIFY((is_same<test_v_uid_t1_ref, type3>::value));
@@ -208,18 +303,46 @@ int main(void)
         VERIFY((is_same<test_cv_uid_t2, type3>::value));
         VERIFY((is_same<test_cv_uid_t1_ref, type3>::value));
         VERIFY((is_same<test_cv_uid_t2_ref, type3>::value));
-        */
     }
 
+#define NO_CV
 
+#define COMMON_TYPE_TEST_ALL_2(type1, type2, type3) \
+  COMMON_TYPE_TEST_2(NO_CV, type1, type2, type3); \
+  COMMON_TYPE_TEST_2(const, type1, type2, type3); \
+  COMMON_TYPE_TEST_2(volatile, type1, type2, type3); \
+  COMMON_TYPE_TEST_2(const volatile, type1, type2, type3)
 
     {
         
-        //COMMON_TYPE_TEST_ALL_2(int, int, int, 1);
-        //COMMON_TYPE_TEST_ALL_2(int, double, double, 2);
-        //COMMON_TYPE_TEST_2(NO_CV, A, A, A, 3);
-        //COMMON_TYPE_TEST_2(const, A, A, A, 4);
-        //COMMON_TYPE_TEST_2(NO_CV, B, A, A, 5);
+        COMMON_TYPE_TEST_ALL_2(int, int, int);
+        COMMON_TYPE_TEST_ALL_2(int, double, double);
+        COMMON_TYPE_TEST_2(NO_CV, A, A, A);
+        COMMON_TYPE_TEST_2(const, A, A, A);
+        STATIC_ASSERT((is_same<common_type<A, B>::type, A>::value), common_type_for_A_and_B_should_be_A);
+        COMMON_TYPE_TEST_2(NO_CV, B, A, A);
     }
+
+    // variadic
+    STATIC_ASSERT((is_same<common_type<A, B, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<B, A, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<C, A, B, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<D, A, B, C>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+
+    STATIC_ASSERT((is_same<common_type<A&, B, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<B&, A, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<C&, A, B, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<D&, A, B, C>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+
+    STATIC_ASSERT((is_same<common_type<A&, B&, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<B&, A&, C, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<C&, A&, B, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<D&, A&, B, C>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+
+    STATIC_ASSERT((is_same<common_type<A, B, C&, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<B, A, C&, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<C, A, B&, D>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+    STATIC_ASSERT((is_same<common_type<D, A, B&, C>::type, A>::value), common_type_for_A_and_B_and_C_and_D_should_be_A);
+
     return 0;
 }
