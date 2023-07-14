a5325b465a3b08ed94efd6825eb06cf90b4e569d
oktonion <0oktonion0@gmail.com>
2023-07-14 16:08:01 +0300

    Create is_convertible.cpp

---
+++ tests/is_convertible.cpp
@@ -0,0 +1,22 @@
+#include "../stdex/include/core.h"
+#include "../stdex/include/type_traits.hpp"
+
+class E { public: template<class T> E(const T&) {} };
+class A {};
+class B : public A {};
+class C {};
+class D { public: operator C() { return c; } C c; };
+
+int main(void)
+{
+    using namespace stdex;
+    
+    STATIC_ASSERT((is_convertible<B*, A*>::value == true), should_be_convertible);
+    STATIC_ASSERT((is_convertible<A*, B*>::value == false), should_not_be_convertible);
+    STATIC_ASSERT((is_convertible<D, C>::value == true), should_be_convertible);
+    STATIC_ASSERT((is_convertible<B*, C*>::value == false), should_not_be_convertible);
+    STATIC_ASSERT((is_convertible<A, E>::value == true), should_be_convertible);
+    STATIC_ASSERT((is_convertible<int, float>::value == true), should_be_convertible);
+    STATIC_ASSERT((is_convertible<int, E>::value == true), should_be_convertible);
+    return 0;
+}
