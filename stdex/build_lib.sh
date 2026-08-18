244a876c0f12ef408d01c61486bf352fc685362e
Aleksandr <oktonion.work@gmail.com>
2026-08-18 11:56:16 +0300

    Update build_lib.sh

--- stdex/build_lib.sh
+++ stdex/build_lib.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-$COMPILER --version
-
 mkdir ./obj
 build_ok=1
 compiler_options="-std=c++98 -O3"
@@ -16,25 +14,51 @@ fi
 case "$(uname -s)" in
 
    Darwin)
-     echo 'Mac OS X'
+     echo 'OS: Mac OS X'
+	 compiler_of_choice=clang++
      ;;
 
    Linux)
-     echo 'Linux'
+     echo 'OS: Linux'
+	 compiler_of_choice=g++
      ;;
 
    CYGWIN*|MINGW32*|MSYS*|MINGW64*)
-     echo 'MS Windows'
+     echo 'OS: MS Windows'
      compiler_options="-I../pthread/ -fms-extensions"
+	 compiler_of_choice=g++
+     ;;
+   
+   QNX)
+     echo 'OS: QNX'
+	 compiler_of_choice=qcc
      ;;
 
    # Add here more strings to compare
    # See correspondence table at the bottom of this answer
 
    *)
-     echo 'other OS' 
+     echo 'OS: unknown' 
+	 compiler_of_choice=g++
      ;;
 esac
+
+if [ -z ${COMPILER} ]; then
+  output=$( (g++ -v) 2>&1)
+  case "$output" in
+    *cannot*|*such*|*directory*)
+	  echo "cannot find 'g++' compiler"
+    ;;
+	*)
+	  compiler_of_choice=g++
+	;;
+  esac
+  
+  echo "COMPILER var is not set, setting to '$compiler_of_choice'"
+  COMPILER=$compiler_of_choice
+fi
+
+$COMPILER -v
 
 for file in ./src/*.cpp; do
   filename=$(basename -- "$file")
