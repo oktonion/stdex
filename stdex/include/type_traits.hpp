704a444aa3af18a609331fdf4cf6ed1a0fd49865
Aleksandr <oktonion.work@gmail.com>
2026-08-20 13:38:17 +0300

    Update type_traits.hpp

--- stdex/include/type_traits.hpp
+++ stdex/include/type_traits.hpp
@@ -3379,7 +3379,7 @@ _STDEX_MSVC_SUPPRESS_WARNING_POP // warning C4180
         >
         struct _common_other_type_impl1_any_value1 {
             _common_other_type_impl1_any_value1(
-                _CommonT[1 + sizeof(false ? ((_declval<_U>())) : ((_declval<_Tp>()))) / sizeof(false ? ((_declval<_Tp>())) : ((_declval<_U>())))]
+                typename add_pointer<_CommonT>::type
             ) {}
         };
 
