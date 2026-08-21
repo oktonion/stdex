e91d7fc3e9c2570ebca686749d1ca259cd0011ea
oktonion <oktonion.work@gmail.com>
2026-08-21 10:01:16 +0300

    Update type_traits.hpp

--- stdex/include/type_traits.hpp
+++ stdex/include/type_traits.hpp
@@ -3371,14 +3371,17 @@ _STDEX_MSVC_SUPPRESS_WARNING_POP // warning C4180
         _Tp _common_other_type_any_value_declval<_Tp>::value;
 
         template<class _Tp, class _U, class _CommonT
-            , const int _Dummy[1 +
-#               if defined(_MSC_VER) // since GCC has internal bug with Itanium mangling for 'sizeof(callable_arg)' and MSVS is bugged beyond infinity this has to be done:
-                sizeof( false ? ((_declval<_U>())) : ((_declval<_Tp>())) ) / sizeof( false ? ((_declval<_Tp>())) : ((_declval<_U>())) )
-#               else
+            , const int 
+#           if defined(_MSC_VER) // since GCC has internal bug with Itanium mangling for 'sizeof(callable_arg)' and MSVS is bugged beyond infinity this has to be done:
+            (*_Dummy)[1 +
                 sizeof( false ? ( (_common_other_type_any_value_declval<_U>::value) ) : ( (_common_other_type_any_value_declval<_Tp>::value) ) ) / 
                 sizeof( false ? ( (_common_other_type_any_value_declval<_Tp>::value) ) : ( (_common_other_type_any_value_declval<_U>::value) ) )
-#               endif
+            - 1] = 0
+#           else
+            _Dummy[1 +
+                sizeof( false ? ((_declval<_U>())) : ((_declval<_Tp>())) ) / sizeof( false ? ((_declval<_Tp>())) : ((_declval<_U>())) )
             - 1] = &integral_constant<int, 0>::value
+#           endif
         >
         struct _common_other_type_impl1_any_value1 {
             _common_other_type_impl1_any_value1(
